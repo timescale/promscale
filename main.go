1a6c794ca302895a40459000d22099af1abca570
Erik Nordström <erik.nordstrom@gmail.com>
2017-04-28 17:31:48 +0200

    Add HTTP server

--- main.go
+++ main.go
@@ -1,13 +1,111 @@
+// Copyright 2017 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The main package for the Prometheus server executable.
 package main
 
+// Based on the Prometheus remote storage example:
+// documentation/examples/remote_storage/remote_storage_adapter/main.go
+
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"sync"
 	"time"
 
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/model"
+
 	"github.com/prometheus/prometheus/storage/remote"
 )
+
+type config struct {
+	timescaledbPassword string
+	timescaledbURL      string
+	remoteTimeout       time.Duration
+	listenAddr          string
+	telemetryPath       string
+}
+
+var (
+	receivedSamples = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "received_samples_total",
+			Help: "Total number of received samples.",
+		},
+	)
+	sentSamples = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "sent_samples_total",
+			Help: "Total number of processed samples sent to remote storage.",
+		},
+		[]string{"remote"},
+	)
+	failedSamples = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "failed_samples_total",
+			Help: "Total number of processed samples which failed on send to remote storage.",
+		},
+		[]string{"remote"},
+	)
+	sentBatchDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "sent_batch_duration_seconds",
+			Help:    "Duration of sample batch send calls to the remote storage.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"remote"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(receivedSamples)
+	prometheus.MustRegister(sentSamples)
+	prometheus.MustRegister(failedSamples)
+	prometheus.MustRegister(sentBatchDuration)
+}
+
+func main() {
+	cfg := parseFlags()
+	http.Handle(cfg.telemetryPath, prometheus.Handler())
+
+	writers, readers := buildClients(cfg)
+	serve(cfg.listenAddr, writers, readers)
+}
+
+func parseFlags() *config {
+	cfg := &config{
+		timescaledbPassword: os.Getenv("TIMESCALEDB_PASSWORD"),
+	}
+
+	flag.StringVar(&cfg.timescaledbURL, "timescaldb-url", "", "The TimescaleDB connect URL")
+	flag.DurationVar(&cfg.remoteTimeout, "send-timeout", 30*time.Second,
+		"The timeout to use when sending samples to the remote storage.",
+	)
+	flag.StringVar(&cfg.listenAddr, "web.listen-address", ":9201", "Address to listen on for web endpoints.")
+	flag.StringVar(&cfg.telemetryPath, "web.telemetry-path", "/metrics", "Address to listen on for web endpoints.")
+
+	flag.Parse()
+
+	return cfg
+}
 
 type writer interface {
 	Write(samples model.Samples) error
@@ -19,22 +117,112 @@ type reader interface {
 	Name() string
 }
 
-type config struct {
-	remoteTimeout time.Duration
-	listenAddr    string
-	telemetryPath string
+func buildClients(cfg *config) ([]writer, []reader) {
+	var writers []writer
+	var readers []reader
+
+	return writers, readers
 }
 
-func parseFlags() *config {
-	cfg := config{}
+func serve(addr string, writers []writer, readers []reader) error {
+	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
+		reqBuf, err := ioutil.ReadAll(snappy.NewReader(r.Body))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-	return &cfg
+		var req remote.WriteRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		samples := protoToSamples(&req)
+		receivedSamples.Add(float64(len(samples)))
+
+		var wg sync.WaitGroup
+		for _, w := range writers {
+			wg.Add(1)
+			go func(rw writer) {
+				sendSamples(rw, samples)
+				wg.Done()
+			}(w)
+		}
+		wg.Wait()
+	})
+
+	http.HandleFunc("/read", func(w http.ResponseWriter, r *http.Request) {
+		reqBuf, err := ioutil.ReadAll(snappy.NewReader(r.Body))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var req remote.ReadRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// TODO: Support reading from more than one reader and merging the results.
+		if len(readers) != 1 {
+			http.Error(w, fmt.Sprintf("expected exactly one reader, found %d readers", len(readers)), http.StatusInternalServerError)
+			return
+		}
+		reader := readers[0]
+
+		var resp *remote.ReadResponse
+		resp, err = reader.Read(&req)
+		if err != nil {
+			log.With("query", req).With("storage", reader.Name()).With("err", err).Warnf("Error executing query")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data, err := proto.Marshal(resp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/x-protobuf")
+		if _, err := snappy.NewWriter(w).Write(data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	return http.ListenAndServe(addr, nil)
 }
 
-func main() {
-	cfg := parseFlags()
-	http.Handle(cfg.telemetryPath, prometheus.Handler())
+func protoToSamples(req *remote.WriteRequest) model.Samples {
+	var samples model.Samples
+	for _, ts := range req.Timeseries {
+		metric := make(model.Metric, len(ts.Labels))
+		for _, l := range ts.Labels {
+			metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+		}
 
-	writers, readers := buildClients(cfg)
-	serve(cfg.listenAddr, writers, readers)
+		for _, s := range ts.Samples {
+			samples = append(samples, &model.Sample{
+				Metric:    metric,
+				Value:     model.SampleValue(s.Value),
+				Timestamp: model.Time(s.TimestampMs),
+			})
+		}
+	}
+	return samples
+}
+
+func sendSamples(w writer, samples model.Samples) {
+	begin := time.Now()
+	err := w.Write(samples)
+	duration := time.Since(begin).Seconds()
+	if err != nil {
+		log.With("num_samples", len(samples)).With("storage", w.Name()).With("err", err).Warnf("Error sending samples to remote storage")
+		failedSamples.WithLabelValues(w.Name()).Add(float64(len(samples)))
+	}
+	sentSamples.WithLabelValues(w.Name()).Add(float64(len(samples)))
+	sentBatchDuration.WithLabelValues(w.Name()).Observe(duration)
 }
