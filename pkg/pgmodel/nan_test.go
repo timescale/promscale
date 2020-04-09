ca5fdab08a06782539b4e231334e32afa0840e0f
Matvey Arye <mat@timescale.com>
2020-04-09 08:39:53 -0400

    Test handling of staleness marker
    Prometheus injects a staleness marker into the data for a series if
    it has not seen the series in a certain amount of time (default 5 min).
    The staleness marker is represented as a float8 NaN with some additional
    bit twiddling.
    
    This PR tests that a staleness marker is correctly propagated to and from
    the database with the bit twiddling intact. It also adds sql functions
    to differentiate a normal NaN from a stale marker.
    
    The alternative is to represent the stale marker as NULL inside the DB,
    but the semantics there aren't clear either and it's further removed
    from the prometheus implementation.

---
+++ pkg/pgmodel/nan_test.go
@@ -0,0 +1,182 @@
+package pgmodel
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/prometheus/prometheus/pkg/value"
+	"github.com/prometheus/prometheus/prompb"
+)
+
+func getSingleSampleValue(t *testing.T, resp *prompb.ReadResponse) float64 {
+	res := resp.GetResults()
+	if len(res) != 1 {
+		t.Fatal("Expect one result")
+	}
+	ts := res[0].GetTimeseries()
+	if len(ts) != 1 {
+		t.Fatal("Expect one timeseries")
+	}
+	samples := ts[0].GetSamples()
+	if len(samples) != 1 {
+		t.Fatal("Expect one sample")
+	}
+	return samples[0].GetValue()
+}
+
+func getBooleanSQLResult(t *testing.T, db *pgxpool.Pool, sql string, args ...interface{}) bool {
+	var res *bool = nil
+	err := db.QueryRow(context.Background(), sql, args...).Scan(&res)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res == nil {
+		t.Fatalf("NULL found")
+	}
+	return *res
+}
+
+func TestSQLStaleNaN(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	withDB(t, *database, func(db *pgxpool.Pool, t *testing.T) {
+		metricName := "StaleMetric"
+		metrics := []prompb.TimeSeries{
+			{
+				Labels: []prompb.Label{
+					{Name: metricNameLabelName, Value: metricName},
+					{Name: "foo", Value: "bar"},
+					{Name: "common", Value: "tag"},
+					{Name: "empty", Value: ""},
+				},
+				Samples: []prompb.Sample{
+					{Timestamp: 10, Value: 0.1},
+					{Timestamp: 20, Value: math.Float64frombits(value.StaleNaN)},
+					{Timestamp: 30, Value: math.NaN()},
+					{Timestamp: 40, Value: 0.4},
+				},
+			},
+		}
+
+		ingestor := NewPgxIngestor(db)
+		defer ingestor.Close()
+		_, err := ingestor.Ingest(metrics)
+
+		if err != nil {
+			t.Fatalf("unexpected error while ingesting test dataset: %s", err)
+		}
+
+		matchers := []*prompb.LabelMatcher{
+			{
+				Type:  prompb.LabelMatcher_EQ,
+				Name:  metricNameLabelName,
+				Value: metricName,
+			},
+		}
+
+		query := []struct {
+			rrq        prompb.ReadRequest
+			isNaN      bool
+			isStaleNaN bool
+		}{
+			{
+				isStaleNaN: true,
+				rrq: prompb.ReadRequest{
+					Queries: []*prompb.Query{
+						{
+							Matchers:         matchers,
+							StartTimestampMs: 19,
+							EndTimestampMs:   21,
+						},
+					},
+				},
+			},
+			{
+				isNaN: true,
+				rrq: prompb.ReadRequest{
+					Queries: []*prompb.Query{
+						{
+							Matchers:         matchers,
+							StartTimestampMs: 29,
+							EndTimestampMs:   31,
+						},
+					},
+				},
+			},
+			{
+				rrq: prompb.ReadRequest{
+					Queries: []*prompb.Query{
+						{
+							Matchers:         matchers,
+							StartTimestampMs: 39,
+							EndTimestampMs:   41,
+						},
+					},
+				},
+			},
+		}
+
+		for _, c := range query {
+			r := NewPgxReader(db)
+			resp, err := r.Read(&c.rrq)
+			startMs := c.rrq.Queries[0].StartTimestampMs
+			endMs := c.rrq.Queries[0].EndTimestampMs
+			timeClause := "time >= 'epoch'::timestamptz + $1 AND time <= 'epoch'::timestamptz + $2"
+
+			if err != nil {
+				t.Fatalf("unexpected error while ingesting test dataset: %s", err)
+			}
+			answer := getSingleSampleValue(t, resp)
+			isStaleNaN := getBooleanSQLResult(t, db,
+				fmt.Sprintf(
+					`SELECT prom.is_stale_marker(value)
+					 FROM prom."StaleMetric"
+					 WHERE %s
+				`, timeClause), time.Duration(int64(time.Millisecond)*startMs), time.Duration(int64(time.Millisecond)*endMs))
+			isNormalNaN := getBooleanSQLResult(t, db,
+				fmt.Sprintf(
+					`SELECT prom.is_normal_nan(value)
+						 FROM prom."StaleMetric"
+						 WHERE %s
+					`, timeClause), time.Duration(int64(time.Millisecond)*startMs), time.Duration(int64(time.Millisecond)*endMs))
+			if c.isStaleNaN {
+				if !value.IsStaleNaN(answer) {
+					t.Fatal("Expected stale NaN, got:", answer)
+				}
+				if !isStaleNaN {
+					t.Fatal("Expected is_stale_marker to return true")
+				}
+			} else {
+				if value.IsStaleNaN(answer) {
+					t.Fatal("Got an unexpected stale NaN")
+				}
+				if isStaleNaN {
+					t.Fatal("Expected is_stale_marker to return false")
+				}
+			}
+			if c.isNaN {
+				if math.Float64bits(answer) != value.NormalNaN {
+					t.Fatal("Expected NaN, got:", answer)
+				}
+				if !isNormalNaN {
+					t.Fatal("Expected is_normal_nan to return true")
+				}
+			} else {
+				if math.Float64bits(answer) == value.NormalNaN {
+					t.Fatal("Got an unexpected NaN")
+				}
+				if isNormalNaN {
+					t.Fatal("Expected is_normal_nan to return false")
+				}
+			}
+		}
+	})
+}
