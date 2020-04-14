shammishailaj/timescale-prometheus

Switch to Array based interchange instead of JSONB
This commit switches the interchange format we use for labels to be two
TEXT[]s instead of a JSONB. This should be a bit more efficient, as we
no longer have to marshal the labels to JSON on the database side, and,
more significantly, have to parse JSON at the adapter.

pkg/pgmodel/migrate_test.go
@@ -555,6 +555,7 @@ func TestSQLJsonLabelArray(t *testing.T) {
 				for _, ts := range c.metrics {
 					labelSet := make(model.LabelSet, len(ts.Labels))
 					metricName := ""
+					kvMap := make(map[string]string)
 					keys := make([]string, 0)
 					values := make([]string, 0)
 					for _, l := range ts.Labels {
@@ -564,6 +565,7 @@ func TestSQLJsonLabelArray(t *testing.T) {
 						labelSet[model.LabelName(l.Name)] = model.LabelValue(l.Value)
 						keys = append(keys, l.Name)
 						values = append(values, l.Value)
+						kvMap[l.Name] = l.Value
 					}
 
 					jsonOrig, err := json.Marshal(labelSet)
@@ -598,19 +600,24 @@ func TestSQLJsonLabelArray(t *testing.T) {
 						t.Fatalf("Expected label arrays to be equal: %v != %v", labelArray, labelArrayKV)
 					}
 
-					var jsonres []byte
-					err = db.QueryRow(context.Background(), "SELECT * FROM prom.label_array_to_jsonb($1)", labelArray).Scan(&jsonres)
+					var (
+						retKeys []string
+						retVals []string
+					)
+					err = db.QueryRow(context.Background(), "SELECT * FROM prom.label_array_to_kv($1)", labelArray).Scan(&retKeys, &retVals)
 					if err != nil {
 						t.Fatal(err)
 					}
-					labelSetRes := make(model.LabelSet, len(ts.Labels))
-					err = json.Unmarshal(jsonres, &labelSetRes)
-					if err != nil {
-						t.Fatal(err)
+					if len(retKeys) != len(retVals) {
+						t.Errorf("invalid kvs, # keys %d, # vals %d", len(retKeys), len(retVals))
 					}
-					if labelSet.Fingerprint() != labelSetRes.Fingerprint() {
-						t.Fatalf("Json not equal: got\n%v\nexpected\n%v", string(jsonres), string(jsonOrig))
-
+					if len(retKeys) != len(kvMap) {
+						t.Errorf("invalid kvs, # keys %d, should be %d", len(retKeys), len(kvMap))
+					}
+					for i, k := range retKeys {
+						if kvMap[k] != retVals[i] {
+							t.Errorf("invalid value for %s\n\tgot\n\t%s\n\twanted\n\t%s", k, retVals[i], kvMap[k])
+						}
 					}
 
 					// Check the series_id logic
@@ -627,19 +634,21 @@ func TestSQLJsonLabelArray(t *testing.T) {
 					if seriesID != seriesIDKeyVal {
 						t.Fatalf("Expected the series ids to be equal: %v != %v", seriesID, seriesIDKeyVal)
 					}
-					err = db.QueryRow(context.Background(), "SELECT prom.label_array_to_jsonb(labels) FROM _prom_catalog.series WHERE id=$1",
-						seriesID).Scan(&jsonres)
+					err = db.QueryRow(context.Background(), "SELECT (prom.label_array_to_kv(labels)).* FROM _prom_catalog.series WHERE id=$1",
+						seriesID).Scan(&retKeys, &retVals)
 					if err != nil {
 						t.Fatal(err)
 					}
-					labelSetRes = make(model.LabelSet, len(ts.Labels))
-					err = json.Unmarshal(jsonres, &labelSetRes)
-					if err != nil {
-						t.Fatal(err)
+					if len(retKeys) != len(retVals) {
+						t.Errorf("invalid kvs, # keys %d, # vals %d", len(retKeys), len(retVals))
 					}
-					if labelSet.Fingerprint() != labelSetRes.Fingerprint() {
-						t.Fatalf("Json not equal: got\n%v\nexpected\n%v", string(jsonres), string(jsonOrig))
-
+					if len(retKeys) != len(kvMap) {
+						t.Errorf("invalid kvs, # keys %d, should be %d", len(retKeys), len(kvMap))
+					}
+					for i, k := range retKeys {
+						if kvMap[k] != retVals[i] {
+							t.Errorf("invalid value for %s\n\tgot\n\t%s\n\twanted\n\t%s", k, retVals[i], kvMap[k])
+						}
 					}
 				}
 			})

pkg/pgmodel/query_builder.go
@@ -5,10 +5,8 @@
 package pgmodel
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"reflect"
 	"sort"
 	"strings"
 	"time"
@@ -35,7 +33,7 @@ const (
 	WHERE %s
 	GROUP BY m.metric_name`
 
-	timeseriesByMetricSQLFormat = `SELECT ` + promSchema + `.label_array_to_jsonb(s.labels), array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
+	timeseriesByMetricSQLFormat = `SELECT (` + promSchema + `.label_array_to_kv(s.labels)).*, array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
 	FROM %s m
 	INNER JOIN _prom_catalog.series s
 	ON m.series_id = s.id
@@ -44,7 +42,7 @@ const (
 	AND time <= '%s'
 	GROUP BY s.id`
 
-	timeseriesBySeriesIDsSQLFormat = `SELECT ` + promSchema + `.label_array_to_jsonb(s.labels), array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
+	timeseriesBySeriesIDsSQLFormat = `SELECT (` + promSchema + `.label_array_to_kv(s.labels)).*, array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
 	FROM %s m
 	INNER JOIN _prom_catalog.series s
 	ON m.series_id = s.id
@@ -188,11 +186,12 @@ func buildTimeSeries(rows pgx.Rows) ([]*prompb.TimeSeries, error) {
 
 	for rows.Next() {
 		var (
+			keys       []string
+			vals       []string
 			timestamps []time.Time
 			values     []float64
-			labels     sampleLabels
 		)
-		err := rows.Scan(&labels, &timestamps, &values)
+		err := rows.Scan(&keys, &vals, &timestamps, &values)
 
 		if err != nil {
 			return nil, err
@@ -202,8 +201,25 @@ func buildTimeSeries(rows pgx.Rows) ([]*prompb.TimeSeries, error) {
 			return nil, fmt.Errorf("query returned a mismatch in timestamps and values")
 		}
 
+		if len(keys) != len(vals) {
+			return nil, fmt.Errorf("query returned a mismatch in label keys and values")
+		}
+
+		promLabels := make([]prompb.Label, 0, len(keys))
+
+		for i, k := range keys {
+			promLabels = append(promLabels, prompb.Label{
+				Name:  k,
+				Value: vals[i],
+			})
+		}
+
+		sort.Slice(promLabels, func(i, j int) bool {
+			return promLabels[i].Name < promLabels[j].Name
+		})
+
 		result := &prompb.TimeSeries{
-			Labels:  labels.ToPrompb(),
+			Labels:  promLabels,
 			Samples: make([]prompb.Sample, 0, len(timestamps)),
 		}
 
@@ -292,70 +308,6 @@ func anchorValue(str string) string {
 	}
 
 	return fmt.Sprintf("^%s$", str)
-}
-
-type sampleLabels struct {
-	JSON        []byte
-	Map         map[string]string
-	OrderedKeys []string
-}
-
-func createOrderedKeys(m map[string]string) []string {
-	keys := make([]string, 0, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	return keys
-}
-
-func (l *sampleLabels) Scan(value interface{}) error {
-	if value == nil {
-		*l = sampleLabels{}
-		return nil
-	}
-
-	var t []byte
-
-	switch v := value.(type) {
-	case string:
-		t = []byte(v)
-	case []byte:
-		t = v
-	default:
-		return errInvalidLabelsValue(reflect.TypeOf(value).String())
-	}
-
-	m := make(map[string]string)
-	err := json.Unmarshal(t, &m)
-
-	if err != nil {
-		return err
-	}
-
-	*l = sampleLabels{
-		JSON:        t,
-		Map:         m,
-		OrderedKeys: createOrderedKeys(m),
-	}
-	return nil
-}
-
-func (l sampleLabels) ToPrompb() []prompb.Label {
-	result := make([]prompb.Label, 0, l.len())
-
-	for _, k := range l.OrderedKeys {
-		result = append(result, prompb.Label{
-			Name:  k,
-			Value: l.Map[k],
-		})
-	}
-
-	return result
-}
-
-func (l *sampleLabels) len() int {
-	return len(l.OrderedKeys)
 }
 
 func toMilis(t time.Time) int64 {

timescale/promscale

pkg/pgmodel/migrate_test.go
@@ -555,6 +555,7 @@ func TestSQLJsonLabelArray(t *testing.T) {
 				for _, ts := range c.metrics {
 					labelSet := make(model.LabelSet, len(ts.Labels))
 					metricName := ""
+					kvMap := make(map[string]string)
 					keys := make([]string, 0)
 					values := make([]string, 0)
 					for _, l := range ts.Labels {
@@ -564,6 +565,7 @@ func TestSQLJsonLabelArray(t *testing.T) {
 						labelSet[model.LabelName(l.Name)] = model.LabelValue(l.Value)
 						keys = append(keys, l.Name)
 						values = append(values, l.Value)
+						kvMap[l.Name] = l.Value
 					}
 
 					jsonOrig, err := json.Marshal(labelSet)
@@ -600,9 +602,6 @@ func TestSQLJsonLabelArray(t *testing.T) {
 
 					var jsonres []byte
 					err = db.QueryRow(context.Background(), "SELECT * FROM prom.label_array_to_jsonb($1)", labelArray).Scan(&jsonres)
-					if err != nil {
-						t.Fatal(err)
-					}
 					labelSetRes := make(model.LabelSet, len(ts.Labels))
 					err = json.Unmarshal(jsonres, &labelSetRes)
 					if err != nil {
@@ -613,12 +612,33 @@ func TestSQLJsonLabelArray(t *testing.T) {
 
 					}
 
+					var (
+						retKeys []string
+						retVals []string
+					)
+					err = db.QueryRow(context.Background(), "SELECT * FROM prom.label_array_to_key_value_array($1)", labelArray).Scan(&retKeys, &retVals)
+					if err != nil {
+						t.Fatal(err)
+					}
+					if len(retKeys) != len(retVals) {
+						t.Errorf("invalid kvs, # keys %d, # vals %d", len(retKeys), len(retVals))
+					}
+					if len(retKeys) != len(kvMap) {
+						t.Errorf("invalid kvs, # keys %d, should be %d", len(retKeys), len(kvMap))
+					}
+					for i, k := range retKeys {
+						if kvMap[k] != retVals[i] {
+							t.Errorf("invalid value for %s\n\tgot\n\t%s\n\twanted\n\t%s", k, retVals[i], kvMap[k])
+						}
+					}
+
 					// Check the series_id logic
 					var seriesID int
 					err = db.QueryRow(context.Background(), "SELECT prom.get_series_id_for_label($1)", jsonOrig).Scan(&seriesID)
 					if err != nil {
 						t.Fatal(err)
 					}
+
 					var seriesIDKeyVal int
 					err = db.QueryRow(context.Background(), "SELECT prom.get_series_id_for_key_value_array($1, $2, $3)", metricName, keys, values).Scan(&seriesIDKeyVal)
 					if err != nil {
@@ -627,19 +647,35 @@ func TestSQLJsonLabelArray(t *testing.T) {
 					if seriesID != seriesIDKeyVal {
 						t.Fatalf("Expected the series ids to be equal: %v != %v", seriesID, seriesIDKeyVal)
 					}
+
 					err = db.QueryRow(context.Background(), "SELECT prom.label_array_to_jsonb(labels) FROM _prom_catalog.series WHERE id=$1",
 						seriesID).Scan(&jsonres)
-					if err != nil {
-						t.Fatal(err)
-					}
 					labelSetRes = make(model.LabelSet, len(ts.Labels))
 					err = json.Unmarshal(jsonres, &labelSetRes)
 					if err != nil {
 						t.Fatal(err)
 					}
+
 					if labelSet.Fingerprint() != labelSetRes.Fingerprint() {
 						t.Fatalf("Json not equal: got\n%v\nexpected\n%v", string(jsonres), string(jsonOrig))
 
+					}
+
+					err = db.QueryRow(context.Background(), "SELECT (prom.label_array_to_key_value_array(labels)).* FROM _prom_catalog.series WHERE id=$1",
+						seriesID).Scan(&retKeys, &retVals)
+					if err != nil {
+						t.Fatal(err)
+					}
+					if len(retKeys) != len(retVals) {
+						t.Errorf("invalid kvs, # keys %d, # vals %d", len(retKeys), len(retVals))
+					}
+					if len(retKeys) != len(kvMap) {
+						t.Errorf("invalid kvs, # keys %d, should be %d", len(retKeys), len(kvMap))
+					}
+					for i, k := range retKeys {
+						if kvMap[k] != retVals[i] {
+							t.Errorf("invalid value for %s\n\tgot\n\t%s\n\twanted\n\t%s", k, retVals[i], kvMap[k])
+						}
 					}
 				}
 			})

pkg/pgmodel/query_builder.go
@@ -5,10 +5,8 @@
 package pgmodel
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"reflect"
 	"sort"
 	"strings"
 	"time"
@@ -35,7 +33,7 @@ const (
 	WHERE %s
 	GROUP BY m.metric_name`
 
-	timeseriesByMetricSQLFormat = `SELECT ` + promSchema + `.label_array_to_jsonb(s.labels), array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
+	timeseriesByMetricSQLFormat = `SELECT (` + promSchema + `.label_array_to_key_value_array(s.labels)).*, array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
 	FROM %s m
 	INNER JOIN _prom_catalog.series s
 	ON m.series_id = s.id
@@ -44,7 +42,7 @@ const (
 	AND time <= '%s'
 	GROUP BY s.id`
 
-	timeseriesBySeriesIDsSQLFormat = `SELECT ` + promSchema + `.label_array_to_jsonb(s.labels), array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
+	timeseriesBySeriesIDsSQLFormat = `SELECT (` + promSchema + `.label_array_to_key_value_array(s.labels)).*, array_agg(m.time ORDER BY time), array_agg(m.value ORDER BY time)
 	FROM %s m
 	INNER JOIN _prom_catalog.series s
 	ON m.series_id = s.id
@@ -188,11 +186,12 @@ func buildTimeSeries(rows pgx.Rows) ([]*prompb.TimeSeries, error) {
 
 	for rows.Next() {
 		var (
+			keys       []string
+			vals       []string
 			timestamps []time.Time
 			values     []float64
-			labels     sampleLabels
 		)
-		err := rows.Scan(&labels, &timestamps, &values)
+		err := rows.Scan(&keys, &vals, &timestamps, &values)
 
 		if err != nil {
 			return nil, err
@@ -202,8 +201,25 @@ func buildTimeSeries(rows pgx.Rows) ([]*prompb.TimeSeries, error) {
 			return nil, fmt.Errorf("query returned a mismatch in timestamps and values")
 		}
 
+		if len(keys) != len(vals) {
+			return nil, fmt.Errorf("query returned a mismatch in label keys and values")
+		}
+
+		promLabels := make([]prompb.Label, 0, len(keys))
+
+		for i, k := range keys {
+			promLabels = append(promLabels, prompb.Label{
+				Name:  k,
+				Value: vals[i],
+			})
+		}
+
+		sort.Slice(promLabels, func(i, j int) bool {
+			return promLabels[i].Name < promLabels[j].Name
+		})
+
 		result := &prompb.TimeSeries{
-			Labels:  labels.ToPrompb(),
+			Labels:  promLabels,
 			Samples: make([]prompb.Sample, 0, len(timestamps)),
 		}
 
@@ -292,70 +308,6 @@ func anchorValue(str string) string {
 	}
 
 	return fmt.Sprintf("^%s$", str)
-}
-
-type sampleLabels struct {
-	JSON        []byte
-	Map         map[string]string
-	OrderedKeys []string
-}
-
-func createOrderedKeys(m map[string]string) []string {
-	keys := make([]string, 0, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	return keys
-}
-
-func (l *sampleLabels) Scan(value interface{}) error {
-	if value == nil {
-		*l = sampleLabels{}
-		return nil
-	}
-
-	var t []byte
-
-	switch v := value.(type) {
-	case string:
-		t = []byte(v)
-	case []byte:
-		t = v
-	default:
-		return errInvalidLabelsValue(reflect.TypeOf(value).String())
-	}
-
-	m := make(map[string]string)
-	err := json.Unmarshal(t, &m)
-
-	if err != nil {
-		return err
-	}
-
-	*l = sampleLabels{
-		JSON:        t,
-		Map:         m,
-		OrderedKeys: createOrderedKeys(m),
-	}
-	return nil
-}
-
-func (l sampleLabels) ToPrompb() []prompb.Label {
-	result := make([]prompb.Label, 0, l.len())
-
-	for _, k := range l.OrderedKeys {
-		result = append(result, prompb.Label{
-			Name:  k,
-			Value: l.Map[k],
-		})
-	}
-
-	return result
-}
-
-func (l *sampleLabels) len() int {
-	return len(l.OrderedKeys)
 }
 
 func toMilis(t time.Time) int64 {