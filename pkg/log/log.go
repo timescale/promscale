232756e587972aa1c05d2d11e61fe0e9bb975ef9
Blagoj Atanasovski <blagoj@timescale.com>
2020-05-20 16:29:37 +0200

    Fix log.go always being the caller in the logs

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -8,31 +8,34 @@ package log
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/prometheus/common/promlog"
 )
 
 var (
 	// Application wide logger
 	logger log.Logger
+
+	// logger timestamp format
+	timestampFormat = log.TimestampFormat(
+		func() time.Time { return time.Now().UTC() },
+		"2006-01-02T15:04:05.000Z07:00",
+	)
 )
 
 // Init starts logging given the minimum log level
 func Init(logLevel string) error {
-	allowedLevel := promlog.AllowedLevel{}
-	err := allowedLevel.Set(logLevel)
+	var l log.Logger
+	l = log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+	logLevelOption, err := parseLogLevel(logLevel)
 	if err != nil {
 		return err
 	}
 
-	config := promlog.Config{
-		Level:  &allowedLevel,
-		Format: &promlog.AllowedFormat{},
-	}
-
-	logger = promlog.New(&config)
+	l = level.NewFilter(l, logLevelOption)
+	logger = log.With(l, "ts", timestampFormat, "caller", log.Caller(4))
 	return nil
 }
 
@@ -70,4 +73,19 @@ type CustomCacheLogger struct{}
 // Printf sends the log in the debug stream of the logger.
 func (c *CustomCacheLogger) Printf(format string, v ...interface{}) {
 	_ = level.Debug(logger).Log("msg", fmt.Sprintf(format, v...))
+}
+
+func parseLogLevel(logLevel string) (level.Option, error) {
+	switch logLevel {
+	case "debug":
+		return level.AllowDebug(), nil
+	case "info":
+		return level.AllowInfo(), nil
+	case "warn":
+		return level.AllowWarn(), nil
+	case "error":
+		return level.AllowError(), nil
+	default:
+		return nil, fmt.Errorf("unrecognized log level %q", logLevel)
+	}
 }
