43115a4b207e341729d9c48c9a293e854d31fd80
Joshua Lockerman <josh@timescale.com>
2020-06-04 17:07:12 -0400

    Add postgres rate functions using a rust extension
    This commit adds implementations of the prometheus rate, increase and delta
    functions to the timescale_prometheus_extra extension. These features are added
    using a rust addition to the extension, based on our timescale-extension-utils
    library.
    
    The functions are a fairly direct post of the relevant prometheus code
    
    Note that while these functions "work" in the sense that the produce output and
    don't crash, they have not been extensively tested against prometheus yet, and
    may still have behavioral differences

--- pkg/pgmodel/end_to_end_tests/functions_test.go
+++ pkg/pgmodel/end_to_end_tests/functions_test.go
@@ -10,6 +10,7 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 
@@ -332,6 +333,187 @@ func TestExtensionFunctions(t *testing.T) {
 			if schema != extSchema {
 				t.Errorf("function %s in wrong schema\nexpected\n\t%s\nfound\n\t%s", opr, extSchema, schema)
 			}
+		}
+	})
+}
+
+func TestExtensionGapfillDelta(t *testing.T) {
+	if !*useExtension || testing.Short() {
+		t.Skip("skipping extension test; testing without extension")
+	}
+	withDB(t, *testDatabase, func(db *pgxpool.Pool, t testing.TB) {
+		_, err := db.Exec(context.Background(), "CREATE TABLE gfd_test_table(t TIMESTAMPTZ, v DOUBLE PRECISION);")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		times := []string{
+			"2000-01-02 15:00:00 UTC",
+			"2000-01-02 15:05:00 UTC",
+			"2000-01-02 15:10:00 UTC",
+			"2000-01-02 15:15:00 UTC",
+			"2000-01-02 15:20:00 UTC",
+			"2000-01-02 15:25:00 UTC",
+			"2000-01-02 15:30:00 UTC",
+			"2000-01-02 15:35:00 UTC",
+			"2000-01-02 15:40:00 UTC",
+			"2000-01-02 15:45:00 UTC",
+		}
+		vals := []float64{0, 50, 100, 150, 200, 200, 150, 100, 50, 0}
+		_, err = db.Exec(context.Background(),
+			"INSERT INTO gfd_test_table SELECT unnest($1::TEXT[])::TIMESTAMPTZ t, unnest($2::DOUBLE PRECISION[]) v;",
+			times, vals)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var res string
+		err = db.QueryRow(context.Background(),
+			"SELECT prom_delta('2000-01-02 15:00:00 UTC'::TIMESTAMPTZ, '2000-01-02 15:45:00 UTC'::TIMESTAMPTZ, 20 * 60 * 1000, 20 * 60 * 1000, NULL, v order by t)::TEXT FROM gfd_test_table;").Scan(&res)
+		if err.Error() != "ERROR: NULL value for non-nullable argument \"time\" (SQLSTATE XX000)" {
+			t.Error(err)
+		}
+		err = db.QueryRow(context.Background(),
+			"SELECT prom_delta('2000-01-02 15:00:00 UTC'::TIMESTAMPTZ, '2000-01-02 15:45:00 UTC'::TIMESTAMPTZ, 20 * 60 * 1000, 20 * 60 * 1000, '2020-01-02 15:00:00 UTC'::TIMESTAMPTZ, v order by t)::TEXT FROM gfd_test_table;").Scan(&res)
+		if err.Error() != "ERROR: input time less than lowest time (SQLSTATE XX000)" {
+			t.Error(err)
+		}
+		err = db.QueryRow(context.Background(),
+			"SELECT prom_delta('2000-01-02 15:00:00 UTC'::TIMESTAMPTZ, '2000-01-02 15:45:00 UTC'::TIMESTAMPTZ, 20 * 60 * 1000, 20 * 60 * 1000, t, v order by t)::TEXT FROM gfd_test_table;").Scan(&res)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if res != "{200,-150}" {
+			t.Errorf("wrong result. Expected\n\t{200,-150}\nfound\n\t%s\n", res)
+		}
+	})
+}
+
+func TestExtensionGapfillIncrease(t *testing.T) {
+	if !*useExtension || testing.Short() {
+		t.Skip("skipping extension test; testing without extension")
+	}
+	startTime, err := time.Parse(time.RFC3339, "2000-01-02T15:00:00Z")
+	if err != nil {
+		t.Fatal(err)
+	}
+	testCases := []struct {
+		name   string
+		times  []time.Time
+		vals   []float64
+		window int64
+		result string
+	}{
+		{
+			name: "basic foo 50m",
+			times: func() []time.Time {
+				times := make([]time.Time, 11)
+				nextTime := startTime
+				for i := range times {
+					times[i] = nextTime
+					nextTime = nextTime.Add(5 * time.Minute)
+				}
+				return times
+			}(),
+			vals: func() []float64 {
+				vals := make([]float64, 11)
+				nextVal := 0.0
+				for i := range vals {
+					vals[i] = nextVal
+					nextVal += 10
+				}
+				return vals
+			}(),
+			window: 50 * 60 * 1000,
+			result: "{100}",
+		},
+		{
+			name: "basic bar 50m",
+			times: func() []time.Time {
+				times := make([]time.Time, 11)
+				nextTime := startTime
+				if err != nil {
+					t.Fatal(err)
+				}
+				for i := range times {
+					times[i] = nextTime
+					nextTime = nextTime.Add(5 * time.Minute)
+				}
+				return times
+			}(),
+			vals: func() []float64 {
+				vals := make([]float64, 11)
+				nextVal := 0.0
+				for i := 0; i <= 5; i++ {
+					vals[i] = nextVal
+					nextVal += 10
+				}
+				nextVal = 0.0
+				for i := 6; i < 11; i++ {
+					vals[i] = nextVal
+					nextVal += 10
+				}
+				return vals
+			}(),
+			window: 50 * 60 * 1000,
+			result: "{90}",
+		},
+		{
+			name: "counter reset",
+			times: func() []time.Time {
+				times := make([]time.Time, 7)
+				nextTime := startTime
+				if err != nil {
+					t.Fatal(err)
+				}
+				for i := range times {
+					times[i] = nextTime
+					nextTime = nextTime.Add(5 * time.Minute)
+				}
+				return times
+			}(),
+			vals:   []float64{0, 1, 2, 3, 2, 3, 4},
+			window: 30 * 60 * 1000,
+			result: "{7}",
+		},
+	}
+	withDB(t, *testDatabase, func(db *pgxpool.Pool, tb testing.TB) {
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+
+				_, err := db.Exec(context.Background(), "CREATE TABLE gfi_test_table(t TIMESTAMPTZ, v DOUBLE PRECISION);")
+				if err != nil {
+					t.Fatal(err)
+				}
+				defer func() {
+					_, err := db.Exec(context.Background(), "DROP TABLE gfi_test_table;")
+					if err != nil {
+						t.Fatal(err)
+					}
+				}()
+
+				_, err = db.Exec(context.Background(),
+					"INSERT INTO gfi_test_table SELECT unnest($1::TIMESTAMPTZ[]) t, unnest($2::DOUBLE PRECISION[]) v;",
+					testCase.times, testCase.vals)
+				if err != nil {
+					t.Fatal(err)
+				}
+				var res string
+				err = db.QueryRow(context.Background(),
+					"SELECT prom_increase($1::TIMESTAMPTZ, $2::TIMESTAMPTZ, $3, $3, t, v order by t)::TEXT FROM gfi_test_table;",
+					startTime, startTime.Add(time.Duration(testCase.window)*time.Millisecond), testCase.window,
+				).Scan(&res)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if res != testCase.result {
+					t.Errorf("wrong result. Expected\n\t%s\nfound\n\t%s\n", testCase.result, res)
+				}
+			})
 		}
 	})
 }
