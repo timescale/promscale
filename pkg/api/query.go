ee7ce9d350d5ae6a9ab08e77e8eda1ae8214502a
Blagoj Atanasovski <blagoj@timescale.com>
2020-07-22 13:41:35 +0200

    Implement Series endpoint of Prom HTTP API
    The series endpoint returns the series for a set of
    matchers. Unit and e2e tests added to confirm compatibility
    with prometheus

--- pkg/api/query.go
+++ pkg/api/query.go
@@ -2,18 +2,10 @@ package api
 
 import (
 	"context"
-	"encoding/json"
-	"io"
-	"math"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/NYTimes/gziphandler"
-	"github.com/pkg/errors"
-	"github.com/prometheus/common/model"
-	"github.com/prometheus/prometheus/promql/parser"
-	"github.com/prometheus/prometheus/storage"
 	"github.com/timescale/timescale-prometheus/pkg/log"
 	"github.com/timescale/timescale-prometheus/pkg/promql"
 	"github.com/timescale/timescale-prometheus/pkg/query"
@@ -76,108 +68,4 @@ func Query(queryEngine *promql.Engine, queryable *query.Queryable) http.Handler 
 	})
 
 	return gziphandler.GzipHandler(hf)
-}
-
-func setHeaders(w http.ResponseWriter, res *promql.Result, warnings storage.Warnings) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/json")
-	if warnings != nil && len(warnings) > 0 {
-		w.Header().Set("Cache-Control", "no-store")
-	}
-	if res != nil && res.Value != nil {
-		w.WriteHeader(http.StatusOK)
-	} else {
-		w.WriteHeader(http.StatusNoContent)
-	}
-}
-
-func respondQuery(w http.ResponseWriter, res *promql.Result, warnings storage.Warnings) {
-	setHeaders(w, res, warnings)
-	switch resVal := res.Value.(type) {
-	case promql.Vector:
-		warnings := make([]string, 0, len(res.Warnings))
-		for _, warn := range res.Warnings {
-			warnings = append(warnings, warn.Error())
-		}
-		_ = marshalVectorResponse(w, resVal, warnings)
-	case promql.Matrix:
-		warnings := make([]string, 0, len(res.Warnings))
-		for _, warn := range res.Warnings {
-			warnings = append(warnings, warn.Error())
-		}
-		_ = marshalMatrixResponse(w, resVal, warnings)
-	default:
-		resp := &response{
-			Status: "success",
-			Data: &queryData{
-				ResultType: res.Value.Type(),
-				Result:     res.Value,
-			},
-		}
-		for _, warn := range res.Warnings {
-			resp.Warnings = append(resp.Warnings, warn.Error())
-		}
-		_ = json.NewEncoder(w).Encode(resp)
-	}
-}
-
-func respondError(w http.ResponseWriter, status int, err error, errType string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Cache-Control", "no-store")
-	w.WriteHeader(status)
-	_ = json.NewEncoder(w).Encode(&errResponse{
-		Status:    "error",
-		ErrorType: errType,
-		Error:     err.Error(),
-	})
-}
-
-type errResponse struct {
-	Status    string `json:"status"`
-	ErrorType string `json:"errorType"`
-	Error     string `json:"error"`
-}
-
-type response struct {
-	Status   string      `json:"status"`
-	Data     interface{} `json:"data,omitempty"`
-	Warnings []string    `json:"warnings,omitempty"`
-}
-
-type queryData struct {
-	ResultType parser.ValueType `json:"resultType"`
-	Result     parser.Value     `json:"result"`
-}
-
-func marshalMatrixResponse(writer io.Writer, data promql.Matrix, warnings []string) error {
-	out := &errorWrapper{writer: writer}
-	marshalCommonHeader(out)
-	marshalMatrixData(out, data)
-	marshalCommonFooter(out, warnings)
-	return out.err
-}
-
-func parseTime(s string) (time.Time, error) {
-	if t, err := strconv.ParseFloat(s, 64); err == nil {
-		s, ns := math.Modf(t)
-		return time.Unix(int64(s), int64(ns*float64(time.Second))), nil
-	}
-	if t, err := time.Parse(time.RFC3339Nano, s); err == nil {
-		return t, nil
-	}
-	return time.Time{}, errors.Errorf("cannot parse %q to a valid timestamp", s)
-}
-
-func parseDuration(s string) (time.Duration, error) {
-	if d, err := strconv.ParseFloat(s, 64); err == nil {
-		ts := d * float64(time.Second)
-		if ts > float64(math.MaxInt64) || ts < float64(math.MinInt64) {
-			return 0, errors.Errorf("cannot parse %q to a valid duration. It overflows int64", s)
-		}
-		return time.Duration(ts), nil
-	}
-	if d, err := model.ParseDuration(s); err == nil {
-		return time.Duration(d), nil
-	}
-	return 0, errors.Errorf("cannot parse %q to a valid duration", s)
 }
---
+++ pkg/api/series_test.go
@@ -0,0 +1,131 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/timescale/timescale-prometheus/pkg/log"
+	"github.com/timescale/timescale-prometheus/pkg/query"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestSeries(t *testing.T) {
+	_ = log.Init("debug")
+	testCases := []struct {
+		name        string
+		querier     *mockQuerier
+		matchers    []string
+		start       string
+		end         string
+		expectCode  int
+		expectError string
+	}{
+		{
+			name:        "match[] is not sent",
+			expectCode:  http.StatusBadRequest,
+			matchers:    []string{},
+			start:       "1",
+			end:         "2",
+			expectError: "bad_data",
+			querier:     &mockQuerier{},
+		}, {
+			name:        "Start is unparsable",
+			expectCode:  http.StatusBadRequest,
+			matchers:    []string{"m"},
+			start:       "unparsable",
+			expectError: "bad_data",
+			querier:     &mockQuerier{},
+		}, {
+			name:        "End is unparsable",
+			expectCode:  http.StatusBadRequest,
+			matchers:    []string{"m"},
+			start:       "1.1",
+			end:         "unparsable",
+			expectError: "bad_data",
+			querier:     &mockQuerier{},
+		}, {
+			name:        "End is before start",
+			expectCode:  http.StatusBadRequest,
+			matchers:    []string{"m"},
+			start:       "1.1",
+			end:         "1",
+			expectError: "bad_data",
+			querier:     &mockQuerier{},
+		}, {
+			name:        "Matcher is unparsable",
+			expectCode:  http.StatusBadRequest,
+			matchers:    []string{"wrong_matcher{"},
+			start:       "1",
+			end:         "2",
+			expectError: "bad_data",
+			querier:     &mockQuerier{},
+		}, {
+			name:        "Select error",
+			start:       "1",
+			end:         "2",
+			expectCode:  http.StatusUnprocessableEntity,
+			expectError: "execution",
+			matchers:    []string{"m"},
+			querier:     &mockQuerier{selectErr: fmt.Errorf("some error")},
+		}, {
+			name:       "All good",
+			start:      "1",
+			end:        "2",
+			expectCode: http.StatusOK,
+			matchers:   []string{"m", `m{a="1"}`},
+			querier:    &mockQuerier{},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			handler := Series(query.NewQueryable(tc.querier))
+			queryUrl := constructSeriesRequest(tc.start, tc.end, tc.matchers)
+			w := doSeriesRequest(t, handler, queryUrl)
+
+			if w.Code != tc.expectCode {
+				t.Errorf("Unexpected HTTP status code received: got %d wanted %d", w.Code, tc.expectCode)
+				return
+			}
+			if tc.expectError != "" {
+				var er errResponse
+				_ = json.NewDecoder(bytes.NewReader(w.Body.Bytes())).Decode(&er)
+				if tc.expectError != er.ErrorType {
+					t.Errorf("expected error of type %s, got %s", tc.expectError, er.ErrorType)
+					return
+				}
+			}
+		})
+
+	}
+
+}
+
+func constructSeriesRequest(start, end string, matchers []string) string {
+	var matcherParams []string
+	for _, m := range matchers {
+		matcherParams = append(matcherParams, "match[]="+m)
+	}
+	match := strings.Join(matcherParams, "&")
+	return fmt.Sprintf(
+		"http://localhost:9090/series?start=%s&end=%s&%s",
+		start, end, match,
+	)
+}
+
+func doSeriesRequest(t *testing.T, queryHandler http.Handler, url string) *httptest.ResponseRecorder {
+	req, err := http.NewRequestWithContext(context.Background(), "GET", url, nil)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	req.Header.Set(
+		"Content-Type",
+		"application/x-www-form-urlencoded; param=value",
+	)
+	w := httptest.NewRecorder()
+	queryHandler.ServeHTTP(w, req)
+	return w
+}
