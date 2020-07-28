8a23f4fd914a72f8d1e01188359aeec53445679d
Matvey Arye <mat@timescale.com>
2020-07-28 11:38:52 -0400

    Add value to compression order by
    This forces min and max metadata to be recorded for the value,
    significantly optimizing queries with quals on the value
    with compressedx data.

--- pkg/pgmodel/migrations/sql/idempotent/base.sql
+++ pkg/pgmodel/migrations/sql/idempotent/base.sql
@@ -73,7 +73,7 @@ BEGIN
             ALTER TABLE SCHEMA_DATA.%I SET (
                 timescaledb.compress,
                 timescaledb.compress_segmentby = 'series_id',
-                timescaledb.compress_orderby = 'time'
+                timescaledb.compress_orderby = 'time, value'
             ); $$, r.table_name);
 
         --chunks where the end time is before now()-1 hour will be compressed
@@ -650,10 +650,10 @@ LANGUAGE PLPGSQL VOLATILE;
 GRANT EXECUTE ON FUNCTION SCHEMA_CATALOG.create_series(int, name, SCHEMA_PROM.label_array) TO prom_writer;
 
 -- There shouldn't be a need to have a read only version of this as we'll use
--- the eq or other matcher functions to find series ids like this. However, 
+-- the eq or other matcher functions to find series ids like this. However,
 -- there are possible use cases that need the series id directly for performance
--- that we might want to see if we need to support, in which case a 
--- read only version might be useful in future. 
+-- that we might want to see if we need to support, in which case a
+-- read only version might be useful in future.
 CREATE OR REPLACE  FUNCTION SCHEMA_CATALOG.get_or_create_series_id(label jsonb)
 RETURNS BIGINT AS $$
 DECLARE
