af7b8b89ee1d289d94956a3b0a26003c7f57a1d6
Matvey Arye <mat@timescale.com>
2020-08-27 17:34:54 -0400

    Fix app version number
    We previously had the version number 0.1.0-beta.3.dev.1 but
    since the version number we are releasing is 0.1.0-beta.3
    the version number should have been 0.1.0-beta.2.dev.1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -13,13 +13,18 @@ var (
 	//    usually represents the number of SQL migrations that happened in the cycle
 	//    (see below) and should start at 0.
 	//
+	// Guidance for increasing versions after release:
+	//    - If releasing a version without a pre-release tag, bump the patch and add `-dev.0`
+	//      pre-release tag.
+	//    - If releasing a version with a pre-release tag, add `.dev.0` to prelease tag. Do not
+	//      bump the existing prelease tag.
 	// Example if releasing 0.1.1, the next development cycle is 0.1.2-dev.0
-	// Example if releasing 0.1.3-beta the next development cycle is 0.1.3-beta.1.dev.0
+	// Example if releasing 0.1.3-beta.1 the next development cycle is 0.1.3-beta.1.dev.0
 	//
 	// When introducing a new SQL migration script, you must bump the version of the app
-	// since an app version must uniquely determine the state o f the schema.
+	// since an app version must uniquely determine the state of the schema.
 	// It is customary to bump the version by incrementing the numeral after
 	// the `dev` tag. The SQL migration script name must correspond to the /new/ version.
-	Version    = "0.1.0-beta.3.dev.1"
+	Version    = "0.1.0-beta.2.dev.1"
 	CommitHash = ""
 )
