ec3d37794d9847bff8b6b32defd945a996fe87ee
Matvey Arye <mat@timescale.com>
2020-09-27 09:07:56 -0400

    Prepare for the 0.1.0-beta.5 release

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -33,7 +33,7 @@ var (
 	// since an app version must uniquely determine the state of the schema.
 	// It is customary to bump the version by incrementing the numeral after
 	// the `dev` tag. The SQL migration script name must correspond to the /new/ version.
-	Version    = "0.1.0-beta.4.dev.1"
+	Version    = "0.1.0-beta.5"
 	CommitHash = ""
 
 	TimescaleVersionRangeString = struct {
