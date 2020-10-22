26be12d47fd4a571cf182f5f7373ba35527d033d
Ante Kresic <ante.kresic@gmail.com>
2020-10-22 15:12:55 +0200

    Add code documentation for query/read path of the connector
    This commit also contains some light refactoring to make the code easier to
    read and understand.

--- pkg/pgmodel/labels.go
+++ pkg/pgmodel/labels.go
@@ -6,7 +6,6 @@ package pgmodel
 
 import (
 	"encoding/binary"
-	"errors"
 	"fmt"
 	"math"
 	"sort"
@@ -179,31 +178,4 @@ func (l *Labels) Less(i, j int) bool {
 func (l *Labels) Swap(i, j int) {
 	l.names[j], l.names[i] = l.names[i], l.names[j]
 	l.values[j], l.values[i] = l.values[i], l.values[j]
-}
-
-// FromLabelMatchers parses protobuf label matchers to Prometheus label matchers.
-// TODO this does not belong here, it should be moved, probably to sql_reader.go
-func FromLabelMatchers(matchers []*prompb.LabelMatcher) ([]*labels.Matcher, error) {
-	result := make([]*labels.Matcher, 0, len(matchers))
-	for _, matcher := range matchers {
-		var mtype labels.MatchType
-		switch matcher.Type {
-		case prompb.LabelMatcher_EQ:
-			mtype = labels.MatchEqual
-		case prompb.LabelMatcher_NEQ:
-			mtype = labels.MatchNotEqual
-		case prompb.LabelMatcher_RE:
-			mtype = labels.MatchRegexp
-		case prompb.LabelMatcher_NRE:
-			mtype = labels.MatchNotRegexp
-		default:
-			return nil, errors.New("invalid matcher type")
-		}
-		matcher, err := labels.NewMatcher(mtype, matcher.Name, matcher.Value)
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, matcher)
-	}
-	return result, nil
 }
