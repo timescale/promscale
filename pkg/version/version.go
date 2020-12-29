75ad0ff92094e1f30e657d5177ff75471a914da7
Vineeth Pothulapati <vineethpothulapati@outlook.com>
2020-12-29 19:08:53 +0530

    bump timescaleDB supported range to <2.1.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -47,7 +47,7 @@ var (
 	TimescaleVersionRangeString = struct {
 		Safe, Warn string
 	}{
-		Safe: ">=1.7.3 <2.0.0",
+		Safe: ">=1.7.3 <2.1.0",
 		Warn: ">=1.7.0 <1.7.3",
 	}
 	timescaleVersionSafeRange = semver.MustParseRange(TimescaleVersionRangeString.Safe)
