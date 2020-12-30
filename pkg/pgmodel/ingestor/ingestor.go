52f400517f63d755719656adbbecfdc1a93b556a
Blagoj Atanasovski <blagoj@timescale.com>
2020-12-30 17:41:15 +0100

    Create an HA client that reads ha locks in db
    - The client only issues queries to the database.
    - HaState has been split into a Service and State.
     - ha.Service checks whether a write should be
       permitted
     - ha.State only keeps the state and makes sure
       it is modified by one routine at a time
    - tryChangeLeader is moved to an async routine
      semaphore is added to check that only one of
      these routines runs at a time
    - No tests have been written and so far hasn't been
      tested in any way whatsoever

--- pkg/pgmodel/ingestor/ingestor.go
+++ pkg/pgmodel/ingestor/ingestor.go
@@ -9,7 +9,6 @@ import (
 	"github.com/timescale/promscale/pkg/clockcache"
 	"github.com/timescale/promscale/pkg/ha"
 	"github.com/timescale/promscale/pkg/pgmodel/cache"
-	"github.com/timescale/promscale/pkg/pgmodel/common/errors"
 	"github.com/timescale/promscale/pkg/pgmodel/model"
 	"github.com/timescale/promscale/pkg/pgxconn"
 	"github.com/timescale/promscale/pkg/prompb"
@@ -20,7 +19,7 @@ type Cfg struct {
 	ReportInterval  int
 	SeriesCacheSize uint64
 	NumCopiers      int
-	HA        bool
+	HA              bool
 }
 
 // DBIngestor ingest the TimeSeries data into Timescale database.
@@ -37,11 +36,18 @@ func NewPgxIngestorWithMetricCache(conn pgxconn.PgxConn, cache cache.MetricCache
 		return nil, err
 	}
 
+	var parser Parser
 	if cfg.HA {
-		return &DBIngestor{db: pi, Parser: ha.NewHAState(&conn)}, nil
+		haService, err := ha.NewHAService(conn)
+		if err != nil {
+			return nil, err
+		}
+		parser = ha.NewHAParser(haService)
+	} else {
+		parser = &DataParser{}
 	}
 
-	return &DBIngestor{db: pi, Parser: &DataParser{}}, nil
+	return &DBIngestor{db: pi, Parser: parser}, nil
 }
 
 // NewPgxIngestor returns a new Ingestor that write to PostgreSQL using PGX
@@ -50,28 +56,19 @@ func NewPgxIngestor(conn pgxconn.PgxConn) (*DBIngestor, error) {
 	return NewPgxIngestorWithMetricCache(conn, c, &Cfg{})
 }
 
-type Parser interface{
-	ParseData([]prompb.TimeSeries, *prompb.WriteRequest) (map[string][]model.SamplesInfo, int, error)
-}
-
 // Ingest transforms and ingests the timeseries data into Timescale database.
 // input:
 //     tts the []Timeseries to insert
 //     req the WriteRequest backing tts. It will be added to our WriteRequest
 //         pool when it is no longer needed.
+// NOTE: req will be added to our WriteRequest pool in this function, it must
+//       not be used afterwards.
 func (i *DBIngestor) Ingest(tts []prompb.TimeSeries, req *prompb.WriteRequest) (uint64, error) {
 	var data map[string][]model.SamplesInfo
 	var totalRows int
 	var err error
 
-
-	data, totalRows, err = i.ParseData(tts, req)
-	// Note data == nil case is to handle samples from non-leader
-	// prometheus instance
-	if err != nil || data == nil {
-		return 0, err
-	}
-
+	data, totalRows, err = i.ParseData(tts)
 	// WriteRequests can contain pointers into the original buffer we deserialized
 	// them out of, and can be quite large in and of themselves. In order to prevent
 	// memory blowup, and to allow faster deserializing, we recycle the WriteRequest
@@ -79,6 +76,12 @@ func (i *DBIngestor) Ingest(tts []prompb.TimeSeries, req *prompb.WriteRequest) (
 	// In order for this to work correctly, any data we wish to keep using (e.g.
 	// samples) must no longer be reachable from req.
 	FinishWriteRequest(req)
+
+	// Note data == nil case is to handle samples from non-leader
+	// prometheus instance
+	if err != nil || data == nil {
+		return 0, err
+	}
 
 	rowsInserted, err := i.db.InsertNewData(data)
 	if err == nil && int(rowsInserted) != totalRows {
@@ -91,49 +94,6 @@ func (i *DBIngestor) Ingest(tts []prompb.TimeSeries, req *prompb.WriteRequest) (
 func (i *DBIngestor) CompleteMetricCreation() error {
 	return i.db.CompleteMetricCreation()
 }
-
-type DataParser struct{}
-
-// Parse data into a set of samplesInfo infos per-metric.
-// returns: map[metric name][]SamplesInfo, total rows to insert
-// NOTE: req will be added to our WriteRequest pool in this function, it must
-//       not be used afterwards.
-func (i *DataParser) ParseData(tts []prompb.TimeSeries, req *prompb.WriteRequest) (map[string][]model.SamplesInfo, int, error) {
-	dataSamples := make(map[string][]model.SamplesInfo)
-	rows := 0
-
-	for i := range tts {
-		t := &tts[i]
-		if len(t.Samples) == 0 {
-			continue
-		}
-
-		// Normalize and canonicalize t.Labels.
-		// After this point t.Labels should never be used again.
-		seriesLabels, metricName, err := model.LabelProtosToLabels(t.Labels)
-		if err != nil {
-			return nil, rows, err
-		}
-		if metricName == "" {
-			return nil, rows, errors.ErrNoMetricName
-		}
-		sample := model.SamplesInfo{
-			Labels:   seriesLabels,
-			SeriesID: -1, // sentinel marking the seriesId as unset
-			Samples:  t.Samples,
-		}
-		rows += len(t.Samples)
-
-		dataSamples[metricName] = append(dataSamples[metricName], sample)
-		// we're going to free req after this, but we still need the samples,
-		// so nil the field
-		t.Samples = nil
-	}
-
-	return dataSamples, rows, nil
-}
-
-
 
 // Close closes the ingestor
 func (i *DBIngestor) Close() {
