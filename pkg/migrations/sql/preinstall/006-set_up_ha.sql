9f051d28c396e8ca494f17675372c0abf38f2f3f
Blagoj Atanasovski <blagoj@timescale.com>
2021-01-27 13:25:51 +0100

    Simplify HA SQL functions

--- pkg/migrations/sql/preinstall/006-set_up_ha.sql
+++ pkg/migrations/sql/preinstall/006-set_up_ha.sql
@@ -1,44 +1,55 @@
-CREATE TABLE SCHEMA_CATALOG.ha_locks
+CREATE TABLE SCHEMA_CATALOG.ha_leases
 (
     cluster_name TEXT PRIMARY KEY,
-    leader       TEXT,
+    leader_name  TEXT,
     lease_start  TIMESTAMPTZ,
     lease_until  TIMESTAMPTZ
 );
 
-CREATE TABLE SCHEMA_CATALOG.ha_locks_log
+CREATE TABLE SCHEMA_CATALOG.ha_leases_logs
 (
     cluster_name TEXT        NOT NULL,
-    leader       TEXT        NOT NULL,
+    leader_name  TEXT        NOT NULL,
     lease_start  TIMESTAMPTZ NOT NULL,
-    lease_until  TIMESTAMPTZ NOT NULL,
-    PRIMARY KEY (cluster_name, lease_start),
-    UNIQUE (cluster_name, leader, lease_start)
+    lease_until  TIMESTAMPTZ,
+    PRIMARY KEY (cluster_name, leader_name, lease_start)
 );
 
 
 -- function that trigger to automatically keep the log calls
-CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.ha_locks_audit_fn()
+CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.ha_leases_audit_fn()
     RETURNS TRIGGER
 AS
 $func$
 BEGIN
-    INSERT INTO SCHEMA_CATALOG.ha_locks_log (cluster_name, leader, lease_start, lease_until)
-    VALUES (NEW.cluster_name, NEW.leader, NEW.lease_start, NEW.lease_until)
-           -- if lease is extended, but leader didn't change
-    ON CONFLICT (cluster_name, leader, lease_start) DO UPDATE SET lease_until=EXCLUDED.lease_until;
-    --no ON CONFLICT (cluster_name, leader) clause as there should never be conflicts
-    -- on the PK and if there are it's an error
+    -- update happened, leader didn't change, just lease bounds -> do nothing
+    IF OLD IS NOT NULL AND OLD.leader_name = NEW.leader_name THEN
+        RETURN NEW;
+    END IF;
+
+    -- leader changed, set lease until to existing log line
+    IF OLD IS NOT NULL AND OLD.leader_name <> NEW.leader_name THEN
+        UPDATE ha_leases_logs
+        SET lease_until = OLD.lease_until
+        WHERE cluster_name = OLD.cluster_name
+          AND leader_name = OLD.leader_name
+          AND lease_until IS NULL;
+    END IF;
+
+    -- insert happened or leader changed and new leader needs to be logged
+    INSERT INTO ha_leases_logs (cluster_name, leader_name, lease_start, lease_until)
+    VALUES (NEW.cluster_name, NEW.leader_name, NEW.lease_start, null);
+
     RETURN NEW;
 END;
 $func$ LANGUAGE plpgsql VOLATILE;
 
 -- trigger to automatically keep the log
-CREATE TRIGGER ha_locks_audit
+CREATE TRIGGER ha_leases_audit
     AFTER INSERT OR UPDATE
-    ON SCHEMA_CATALOG.ha_locks
+    ON SCHEMA_CATALOG.ha_leases
     FOR EACH ROW
-EXECUTE PROCEDURE SCHEMA_CATALOG.ha_locks_audit_fn();
+EXECUTE PROCEDURE SCHEMA_CATALOG.ha_leases_audit_fn();
 
 -- default values for lease
 INSERT INTO SCHEMA_CATALOG.default(key, value)
@@ -47,100 +58,86 @@ VALUES ('ha_lease_timeout', '1m'),
 ON CONFLICT (key) DO UPDATE SET value = EXCLUDED.value;
 
 -- ha api functions
-CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.check_insert(cluster TEXT, writer TEXT, min_time TIMESTAMPTZ,
-                                                       max_time TIMESTAMPTZ) RETURNS SETOF ha_locks
+CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.update_lease(cluster TEXT, writer TEXT, min_time TIMESTAMPTZ,
+                                                       max_time TIMESTAMPTZ, lease_timeout INTERVAL DEFAULT '1m',
+                                                       lease_refresh INTERVAL DEFAULT '10s') RETURNS ha_leases
 AS
 $func$
 DECLARE
     leader            TEXT;
     lease_start       TIMESTAMPTZ;
     lease_until       TIMESTAMPTZ;
-    lease_timeout     INTERVAL;
     new_lease_timeout TIMESTAMPTZ;
-    lease_refresh     INTERVAL;
+    lease_state       ha_leases%ROWTYPE;
 BEGIN
-    -- find lease_timeout setting;
-    SELECT value::INTERVAL
-    INTO lease_timeout
-    FROM SCHEMA_CATALOG.default
-    WHERE key = 'ha_lease_timeout';
 
     -- find latest leader and their lease time range;
-    SELECT h.leader, h.lease_start, h.lease_until
+    SELECT h.leader_name, h.lease_start, h.lease_until
     INTO leader, lease_start, lease_until
-    FROM SCHEMA_CATALOG.ha_locks as h
+    FROM SCHEMA_CATALOG.ha_leases as h
     WHERE cluster_name = cluster;
 
     --only happens on very first call;
     IF NOT FOUND THEN
         -- no leader yet for cluster insert;
-        INSERT INTO SCHEMA_CATALOG.ha_locks
+        INSERT INTO SCHEMA_CATALOG.ha_leases
         VALUES (cluster, writer, min_time, max_time + lease_timeout)
         ON CONFLICT DO NOTHING;
         -- needed due to on-conflict clause;
-        SELECT h.leader, h.lease_start, h.lease_until
+        SELECT h.leader_name, h.lease_start, h.lease_until
         INTO leader, lease_start, lease_until
-        FROM SCHEMA_CATALOG.ha_locks as h
+        FROM SCHEMA_CATALOG.ha_leases as h
         WHERE cluster_name = cluster;
     END IF;
 
-    IF leader <> writer OR lease_start > min_time THEN
-        RAISE EXCEPTION 'LEADER_HAS_CHANGED';
+    IF leader <> writer THEN
+        RAISE EXCEPTION 'LEADER_HAS_CHANGED' USING ERRCODE = 'PS010';
     END IF;
-
-    SELECT value::INTERVAL
-    INTO lease_refresh
-    FROM SCHEMA_CATALOG.default
-    WHERE key = 'ha_lease_refresh';
 
     new_lease_timeout = max_time + lease_timeout;
     IF new_lease_timeout > lease_until + lease_refresh THEN
-        UPDATE SCHEMA_CATALOG.ha_locks h
+        UPDATE SCHEMA_CATALOG.ha_leases h
         SET lease_until = new_lease_timeout
         WHERE h.cluster_name = cluster
-          AND h.leader = writer
+          AND h.leader_name = writer
           AND h.lease_until + lease_refresh < new_lease_timeout;
         IF NOT FOUND THEN -- concurrent update
-            SELECT h.leader, h.lease_start, h.lease_until
+            SELECT h.leader_name, h.lease_start, h.lease_until
             INTO leader, lease_start, lease_until
-            FROM SCHEMA_CATALOG.ha_locks as h
+            FROM SCHEMA_CATALOG.ha_leases as h
             WHERE cluster_name = cluster;
-            IF leader <> writer OR lease_start > min_time OR lease_until < max_time
+            IF leader <> writer OR lease_until < max_time
             THEN
-                RAISE EXCEPTION 'LEADER_HAS_CHANGED';
+                RAISE EXCEPTION 'LEADER_HAS_CHANGED' USING ERRCODE = 'PS010';
             END IF;
         END IF;
     END IF;
-    RETURN QUERY SELECT *
-                 FROM ha_locks
-                 WHERE cluster_name = cluster;
+    SELECT * INTO lease_state FROM ha_leases WHERE cluster_name = cluster;
+    RETURN lease_state;
 END;
 $func$ LANGUAGE plpgsql VOLATILE;
 
 CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.try_change_leader(cluster TEXT, new_leader TEXT,
-                                                            max_time TIMESTAMPTZ) RETURNS SETOF ha_locks
+                                                            max_time TIMESTAMPTZ,
+                                                            lease_timeout INTERVAL DEFAULT '1m') RETURNS ha_leases
 AS
 $func$
 DECLARE
-    lease_timeout INTERVAL;
+    lease_state ha_leases%ROWTYPE;
 BEGIN
-    -- find lease_timeout setting;
-    SELECT value::INTERVAL
-    INTO lease_timeout
-    FROM SCHEMA_CATALOG.default
-    WHERE key = 'ha_lease_timeout';
-
-    UPDATE SCHEMA_CATALOG.ha_locks
-    SET leader      = new_leader,
+    UPDATE SCHEMA_CATALOG.ha_leases
+    SET leader_name = new_leader,
         lease_start = lease_until,
         lease_until = max_time + lease_timeout
     WHERE cluster_name = cluster
       AND lease_until < max_time;
 
-    RETURN QUERY
-        SELECT *
-        FROM ha_locks
-        WHERE cluster_name = cluster;
+
+    SELECT *
+    INTO lease_state
+    FROM ha_leases
+    WHERE cluster_name = cluster;
+    RETURN lease_state;
 
 END;
 $func$ LANGUAGE plpgsql VOLATILE;
