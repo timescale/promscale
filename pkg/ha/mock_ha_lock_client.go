f0ee988a145916855dee011bbe8f075552bea502
Vineeth Pothulapati <vineethpothulapati@outlook.com>
2021-01-27 17:42:24 +0530

    restructure HA package + iterate over review comments

--- pkg/ha/mock_ha_lock_client.go
+++ pkg/ha/mock_ha_lock_client.go
@@ -4,48 +4,53 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/jackc/pgconn"
+	"github.com/timescale/promscale/pkg/ha/state"
 )
 
 type mockLockClient struct {
-	leadersPerCluster map[string]*haLockState
+	leadersPerCluster map[string]*state.HALockState
 }
 
-func (m *mockLockClient) checkInsert(_ context.Context, cluster, leader string, minTime, maxTime time.Time) (*haLockState, error) {
+func (m *mockLockClient) updateLease(_ context.Context, cluster, leader string, minTime, maxTime time.Time) (*state.HALockState, error) {
 	lock, exists := m.leadersPerCluster[cluster]
 	if !exists {
-		lock = &haLockState{
-			cluster:    cluster,
-			leader:     leader,
-			leaseStart: minTime,
-			leaseUntil: maxTime,
+		lock = &state.HALockState{
+			Cluster:    cluster,
+			Leader:     leader,
+			LeaseStart: minTime,
+			LeaseUntil: maxTime,
 		}
 		m.leadersPerCluster[cluster] = lock
 	}
 
 	if cluster == "cluster4" || cluster == "cluster5" {
-		return lock, leaderHasChanged
+		p := &pgconn.PgError{}
+		p.Code = "PS010"
+		return lock, p
 	}
 
 	return lock, nil
 }
 
-func (m *mockLockClient) tryChangeLeader(_ context.Context, cluster, newLeader string, maxTime time.Time) (*haLockState, error) {
+func (m *mockLockClient) tryChangeLeader(_ context.Context, cluster, newLeader string, maxTime time.Time) (*state.HALockState, error) {
 	lock, exists := m.leadersPerCluster[cluster]
 	if !exists {
 		return nil, fmt.Errorf("no leader for %s, checkInsert never called before tryChange leader", cluster)
 	}
-	lock = &haLockState{
-		cluster:    cluster,
-		leader:     newLeader,
-		leaseStart: lock.leaseUntil,
-		leaseUntil: maxTime,
+	lock = &state.HALockState{
+		Cluster:    cluster,
+		Leader:     newLeader,
+		LeaseStart: lock.LeaseUntil,
+		LeaseUntil: maxTime,
 	}
 	m.leadersPerCluster[cluster] = lock
 	return lock, nil
 }
 
 func newMockLockClient() *mockLockClient {
-	return &mockLockClient{leadersPerCluster: make(map[string]*haLockState)}
+	return &mockLockClient{leadersPerCluster: make(map[string]*state.HALockState)}
 }
 
 func (m *mockLockClient) readLeaseSettings(ctx context.Context) (timeout, refresh time.Duration, err error) {
@@ -54,18 +59,18 @@ func (m *mockLockClient) readLeaseSettings(ctx context.Context) (timeout, refres
 
 var count int
 
-func (m *mockLockClient) readLockState(ctx context.Context, cluster string) (*haLockState, error) {
+func (m *mockLockClient) readLockState(ctx context.Context, cluster string) (*state.HALockState, error) {
 	count++
 
 	// the below cases are added to simulate the scenario's the leader has updated by another
 	// promscale now the current promscale should adopt to the change.
 	if cluster == "cluster4" {
-		m.leadersPerCluster[cluster].leader = "replica2"
+		m.leadersPerCluster[cluster].Leader = "replica2"
 	}
 
 	if cluster == "cluster5" && count == 1 {
-		m.leadersPerCluster[cluster].leader = "replica2"
-		m.leadersPerCluster[cluster].leaseUntil = time.Now().Add(-10 * time.Minute)
+		m.leadersPerCluster[cluster].Leader = "replica2"
+		m.leadersPerCluster[cluster].LeaseUntil = time.Now().Add(-10 * time.Minute)
 	}
 
 	return m.leadersPerCluster[cluster], nil
---
+++ pkg/ha/state/state.go
@@ -0,0 +1,77 @@
+package state
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type State struct {
+	leader            string
+	leaseStart        time.Time
+	leaseUntil        time.Time
+	maxTimeSeen       time.Time // max data time seen by any instance
+	maxTimeInstance   string    // the instance name that’s seen the maxtime
+	maxTimeSeenLeader time.Time
+	_mu               sync.RWMutex
+}
+
+type StateView struct {
+	Leader            string
+	LeaseStart        time.Time
+	LeaseUntil        time.Time
+	MaxTimeSeen       time.Time
+	MaxTimeInstance   string
+	MaxTimeSeenLeader time.Time
+}
+
+// haLockState represents the current lock holder
+// as reported from the db
+type HALockState struct {
+	Cluster    string
+	Leader     string
+	LeaseStart time.Time
+	LeaseUntil time.Time
+}
+
+func (h *State) UpdateStateFromDB(latestState *HALockState, maxT time.Time, replicaName string) {
+	fmt.Println("latest state: ", latestState)
+	h._mu.Lock()
+	defer h._mu.Unlock()
+	h.leader = latestState.Leader
+	h.leaseStart = latestState.LeaseStart
+	h.leaseUntil = latestState.LeaseUntil
+	h.updateMaxTimeOnZero(maxT, replicaName)
+}
+
+func (h *State) updateMaxTimeOnZero(maxT time.Time, replicaName string) {
+	if h.maxTimeSeen.IsZero() {
+		h.maxTimeSeen = maxT
+		h.maxTimeInstance = replicaName
+	}
+}
+
+func (h *State) UpdateMaxSeenTime(currentReplica string, currentMaxT time.Time) {
+	h._mu.Lock()
+	defer h._mu.Unlock()
+	if currentMaxT.After(h.maxTimeSeen) {
+		h.maxTimeInstance = currentReplica
+		h.maxTimeSeen = currentMaxT
+	}
+
+	if currentMaxT.After(h.maxTimeSeenLeader) && currentReplica == h.leader {
+		h.maxTimeSeenLeader = currentMaxT
+	}
+}
+
+func (h *State) Clone() *StateView {
+	h._mu.RLock()
+	defer h._mu.RUnlock()
+	return &StateView{
+		Leader:          h.leader,
+		LeaseStart:      h.leaseStart,
+		LeaseUntil:      h.leaseUntil,
+		MaxTimeSeen:     h.maxTimeSeen,
+		MaxTimeInstance: h.maxTimeInstance,
+	}
+}
