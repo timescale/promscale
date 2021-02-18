f3481dbb8807a1738b0421050b2307bdd8c670e8
Matvey Arye <mat@timescale.com>
2021-02-17 19:22:45 -0500

    Fix memory leak in Protobuf
    This fixes a leak introduced in commit 4adc89 (PR #256).

--- pkg/prompb/custom.ts.go
+++ pkg/prompb/custom.ts.go
@@ -4,3 +4,6 @@ func (m *Labels) Reset() { *m = Labels{Labels: m.Labels[:0]} }
 func (m *WriteRequest) Reset() {
 	*m = WriteRequest{Timeseries: m.Timeseries[:0], Metadata: m.Metadata[:0]}
 }
+func (m *TimeSeries) Reset() {
+	*m = TimeSeries{Labels: m.Labels[:0], Samples: m.Samples[:0]}
+}
