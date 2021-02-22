1563e0cd6102176b3c1bb18a8ab28fac5e6bdf93
Harkishen-Singh <harkishensingh@hotmail.com>
2021-02-22 13:26:34 +0530

    Rename block -> slab in prom-migrator.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- pkg/migration-tool/integration_tests/integration_test.go
+++ pkg/migration-tool/integration_tests/integration_test.go
@@ -34,7 +34,7 @@ func TestReaderWriterPlannerIntegrationWithoutHalts(t *testing.T) {
 		progressMetricURL  string
 		progressMetricName string
 		progressEnabled    bool
-		maxBlockSizeBytes  int64
+		maxSlabSizeBytes   int64
 	}{
 		name:               "ci-migration",
 		mint:               tsMint,
@@ -46,19 +46,19 @@ func TestReaderWriterPlannerIntegrationWithoutHalts(t *testing.T) {
 		progressMetricName: "progress_metric",
 		progressEnabled:    false,
 		concurrentPulls:    2,
-		maxBlockSizeBytes:  500 * utils.Megabyte,
+		maxSlabSizeBytes:   500 * utils.Megabyte,
 	}
 
 	// Replicate main.
 	planConfig := &plan.Config{
-		Mint:                conf.mint,
-		Maxt:                conf.maxt,
-		JobName:             conf.name,
-		BlockSizeLimitBytes: conf.maxBlockSizeBytes,
-		NumStores:           conf.concurrentPulls,
-		ProgressEnabled:     conf.progressEnabled,
-		ProgressMetricName:  conf.progressMetricName,
-		ProgressMetricURL:   conf.progressMetricURL,
+		Mint:               conf.mint,
+		Maxt:               conf.maxt,
+		JobName:            conf.name,
+		SlabSizeLimitBytes: conf.maxSlabSizeBytes,
+		NumStores:          conf.concurrentPulls,
+		ProgressEnabled:    conf.progressEnabled,
+		ProgressMetricName: conf.progressMetricName,
+		ProgressMetricURL:  conf.progressMetricURL,
 	}
 	planner, proceed, err := plan.Init(planConfig)
 	if err != nil {
@@ -72,14 +72,14 @@ func TestReaderWriterPlannerIntegrationWithoutHalts(t *testing.T) {
 	var (
 		readErrChan  = make(chan error)
 		writeErrChan = make(chan error)
-		sigBlockRead = make(chan *plan.Block)
+		sigSlabRead  = make(chan *plan.Slab)
 	)
 	cont, cancelFunc := context.WithCancel(context.Background())
-	read, err := reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigBlockRead)
+	read, err := reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigSlabRead)
 	if err != nil {
 		t.Fatal("msg", "could not create reader", "error", err.Error())
 	}
-	write, err := writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigBlockRead)
+	write, err := writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigSlabRead)
 	if err != nil {
 		t.Fatal("msg", "could not create writer", "error", err.Error())
 	}
@@ -114,7 +114,7 @@ loop:
 	}
 	// Verify the progress metric samples count.
 	if remoteWriteStorage.SamplesProgress() != 0 {
-		t.Fatalf("progress-metric samples count do not match the number of blocks created: samples: %d and blocks created: %d", remoteWriteStorage.SamplesProgress(), write.Blocks())
+		t.Fatalf("progress-metric samples count do not match the number of slabs created: samples: %d and slabs created: %d", remoteWriteStorage.SamplesProgress(), write.Slabs())
 	}
 }
 
@@ -134,7 +134,7 @@ func TestReaderWriterPlannerIntegrationWithHalt(t *testing.T) {
 		progressMetricURL  string
 		progressMetricName string
 		progressEnabled    bool
-		maxBlockSizeBytes  int64
+		maxSlabSizeBytes   int64
 		concurrentPulls    int
 	}{
 		name:               "ci-migration",
@@ -147,19 +147,19 @@ func TestReaderWriterPlannerIntegrationWithHalt(t *testing.T) {
 		progressMetricName: "progress_metric",
 		progressEnabled:    true,
 		concurrentPulls:    2,
-		maxBlockSizeBytes:  500 * utils.Megabyte,
+		maxSlabSizeBytes:   500 * utils.Megabyte,
 	}
 
 	// Replicate main.
 	planConfig := &plan.Config{
-		Mint:                conf.mint,
-		Maxt:                conf.maxt,
-		JobName:             conf.name,
-		BlockSizeLimitBytes: conf.maxBlockSizeBytes,
-		NumStores:           conf.concurrentPulls,
-		ProgressEnabled:     conf.progressEnabled,
-		ProgressMetricName:  conf.progressMetricName,
-		ProgressMetricURL:   conf.progressMetricURL,
+		Mint:               conf.mint,
+		Maxt:               conf.maxt,
+		JobName:            conf.name,
+		SlabSizeLimitBytes: conf.maxSlabSizeBytes,
+		NumStores:          conf.concurrentPulls,
+		ProgressEnabled:    conf.progressEnabled,
+		ProgressMetricName: conf.progressMetricName,
+		ProgressMetricURL:  conf.progressMetricURL,
 	}
 	planner, proceed, err := plan.Init(planConfig)
 	if err != nil {
@@ -173,15 +173,15 @@ func TestReaderWriterPlannerIntegrationWithHalt(t *testing.T) {
 	var (
 		readErrChan  = make(chan error)
 		writeErrChan = make(chan error)
-		sigBlockRead = make(chan *plan.Block)
+		sigRead      = make(chan *plan.Slab)
 	)
 	cont, cancelFunc := context.WithCancel(context.Background())
-	read, err := reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigBlockRead)
+	read, err := reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create reader", "error", err.Error())
 	}
-	read.SigForceStop = make(chan struct{})
-	write, err := writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigBlockRead)
+	read.SigSlabStop = make(chan struct{})
+	write, err := writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create writer", "error", err.Error())
 	}
@@ -190,10 +190,10 @@ func TestReaderWriterPlannerIntegrationWithHalt(t *testing.T) {
 	write.Run(writeErrChan)
 
 	time.Sleep(time.Millisecond * 100)
-	read.SigForceStop <- struct{}{}
+	read.SigSlabStop <- struct{}{}
 	time.Sleep(time.Millisecond * 100)
 	cancelFunc()
-	previousWriteBlocks := write.Blocks()
+	previousWriteSlabs := write.Slabs()
 
 	planner, proceed, err = plan.Init(planConfig)
 	if err != nil {
@@ -206,14 +206,14 @@ func TestReaderWriterPlannerIntegrationWithHalt(t *testing.T) {
 
 	readErrChan = make(chan error)
 	writeErrChan = make(chan error)
-	sigBlockRead = make(chan *plan.Block)
+	sigRead = make(chan *plan.Slab)
 
 	cont, cancelFunc = context.WithCancel(context.Background())
-	read, err = reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigBlockRead)
+	read, err = reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create reader", "error", err.Error())
 	}
-	write, err = writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigBlockRead)
+	write, err = writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create writer", "error", err.Error())
 	}
@@ -243,16 +243,16 @@ loop:
 		t.Fatalf("read-storage series and write-storage series do not match: read-storage series: %d and write-storage series: %d", remoteReadStorage.Series(), remoteWriteStorage.Series()-1)
 	}
 	// Verify net samples count.
-	if remoteReadStorage.Samples() != remoteWriteStorage.Samples()-int(write.Blocks()+previousWriteBlocks) {
-		t.Fatalf("read-storage samples and write-storage samples do not match: read-storage samples: %d and write-storage samples: %d", remoteReadStorage.Samples(), remoteWriteStorage.Samples()-int(write.Blocks()+previousWriteBlocks))
+	if remoteReadStorage.Samples() != remoteWriteStorage.Samples()-int(write.Slabs()+previousWriteSlabs) {
+		t.Fatalf("read-storage samples and write-storage samples do not match: read-storage samples: %d and write-storage samples: %d", remoteReadStorage.Samples(), remoteWriteStorage.Samples()-int(write.Slabs()+previousWriteSlabs))
 	}
 	// Verify the progress metric samples count.
-	if remoteWriteStorage.SamplesProgress() != int(write.Blocks()+previousWriteBlocks) {
-		t.Fatalf("progress-metric samples count do not match the number of blocks created: progress metric samples: %d and write blocks: %d", remoteWriteStorage.SamplesProgress(), write.Blocks())
+	if remoteWriteStorage.SamplesProgress() != int(write.Slabs()+previousWriteSlabs) {
+		t.Fatalf("progress-metric samples count do not match the number of slabs created: progress metric samples: %d and write slabs: %d", remoteWriteStorage.SamplesProgress(), write.Slabs())
 	}
 }
 
-func TestReaderWriterPlannerIntegrationWithHaltWithBlockSizeOverflow(t *testing.T) {
+func TestReaderWriterPlannerIntegrationWithHaltWithSlabSizeOverflow(t *testing.T) {
 	var largeTimeSeries, tsMint, tsMaxt = generateVeryLargeTimeseries()
 	remoteReadStorage, readURL := createRemoteReadServer(t, largeTimeSeries)
 	defer remoteReadStorage.Close()
@@ -269,7 +269,7 @@ func TestReaderWriterPlannerIntegrationWithHaltWithBlockSizeOverflow(t *testing.
 		progressMetricURL  string
 		progressMetricName string
 		progressEnabled    bool
-		maxBlockSizeBytes  int64
+		maxSlabSizeBytes   int64
 		concurrentPulls    int
 	}{
 		name:               "ci-migration",
@@ -282,20 +282,20 @@ func TestReaderWriterPlannerIntegrationWithHaltWithBlockSizeOverflow(t *testing.
 		progressMetricName: "progress_metric",
 		progressEnabled:    true,
 		concurrentPulls:    1,
-		maxBlockSizeBytes:  50 * 1024,
+		maxSlabSizeBytes:   50 * 1024,
 	}
 
 	// Replicate main.
 	// Replicate main.
 	planConfig := &plan.Config{
-		Mint:                conf.mint,
-		Maxt:                conf.maxt,
-		JobName:             conf.name,
-		BlockSizeLimitBytes: conf.maxBlockSizeBytes,
-		NumStores:           conf.concurrentPulls,
-		ProgressEnabled:     conf.progressEnabled,
-		ProgressMetricName:  conf.progressMetricName,
-		ProgressMetricURL:   conf.progressMetricURL,
+		Mint:               conf.mint,
+		Maxt:               conf.maxt,
+		JobName:            conf.name,
+		SlabSizeLimitBytes: conf.maxSlabSizeBytes,
+		NumStores:          conf.concurrentPulls,
+		ProgressEnabled:    conf.progressEnabled,
+		ProgressMetricName: conf.progressMetricName,
+		ProgressMetricURL:  conf.progressMetricURL,
 	}
 	planner, proceed, err := plan.Init(planConfig)
 	planner.TestCheckFunc = func() {
@@ -314,15 +314,15 @@ func TestReaderWriterPlannerIntegrationWithHaltWithBlockSizeOverflow(t *testing.
 	var (
 		readErrChan  = make(chan error)
 		writeErrChan = make(chan error)
-		sigBlockRead = make(chan *plan.Block)
+		sigRead      = make(chan *plan.Slab)
 	)
 	cont, cancelFunc := context.WithCancel(context.Background())
-	read, err := reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigBlockRead)
+	read, err := reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create reader", "error", err.Error())
 	}
-	read.SigForceStop = make(chan struct{})
-	write, err := writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigBlockRead)
+	read.SigSlabStop = make(chan struct{})
+	write, err := writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create writer", "error", err.Error())
 	}
@@ -331,10 +331,10 @@ func TestReaderWriterPlannerIntegrationWithHaltWithBlockSizeOverflow(t *testing.
 	write.Run(writeErrChan)
 
 	time.Sleep(time.Millisecond * 100)
-	read.SigForceStop <- struct{}{}
+	read.SigSlabStop <- struct{}{}
 	time.Sleep(time.Millisecond * 100)
 	cancelFunc()
-	previousWriteBlocks := write.Blocks()
+	previousWriteSlabs := write.Slabs()
 
 	planner, proceed, err = plan.Init(planConfig)
 	if err != nil {
@@ -347,14 +347,14 @@ func TestReaderWriterPlannerIntegrationWithHaltWithBlockSizeOverflow(t *testing.
 
 	readErrChan = make(chan error)
 	writeErrChan = make(chan error)
-	sigBlockRead = make(chan *plan.Block)
+	sigRead = make(chan *plan.Slab)
 
 	cont, cancelFunc = context.WithCancel(context.Background())
-	read, err = reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigBlockRead)
+	read, err = reader.New(cont, conf.readURL, planner, conf.concurrentPulls, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create reader", "error", err.Error())
 	}
-	write, err = writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigBlockRead)
+	write, err = writer.New(cont, conf.writeURL, conf.progressMetricName, conf.name, conf.numShards, conf.progressEnabled, sigRead)
 	if err != nil {
 		t.Fatal("msg", "could not create writer", "error", err.Error())
 	}
@@ -384,11 +384,11 @@ loop:
 		t.Fatalf("read-storage series and write-storage series do not match: read-storage series: %d and write-storage series: %d", remoteReadStorage.Series(), remoteWriteStorage.Series()-1)
 	}
 	// Verify net samples count.
-	if remoteReadStorage.Samples() != remoteWriteStorage.Samples()-int(write.Blocks()+previousWriteBlocks) {
-		t.Fatalf("read-storage samples and write-storage samples do not match: read-storage samples: %d and write-storage samples: %d", remoteReadStorage.Samples(), remoteWriteStorage.Samples()-int(write.Blocks()+previousWriteBlocks))
+	if remoteReadStorage.Samples() != remoteWriteStorage.Samples()-int(write.Slabs()+previousWriteSlabs) {
+		t.Fatalf("read-storage samples and write-storage samples do not match: read-storage samples: %d and write-storage samples: %d", remoteReadStorage.Samples(), remoteWriteStorage.Samples()-int(write.Slabs()+previousWriteSlabs))
 	}
 	// Verify the progress metric samples count.
-	if remoteWriteStorage.SamplesProgress() != int(write.Blocks()+previousWriteBlocks) {
-		t.Fatalf("progress-metric samples count do not match the number of blocks created: progress metric samples: %d and write blocks: %d", remoteWriteStorage.SamplesProgress(), write.Blocks())
+	if remoteWriteStorage.SamplesProgress() != int(write.Slabs()+previousWriteSlabs) {
+		t.Fatalf("progress-metric samples count do not match the number of slabs created: progress metric samples: %d and write slabs: %d", remoteWriteStorage.SamplesProgress(), write.Slabs())
 	}
 }
--- pkg/migration-tool/planner/slab.go
+++ pkg/migration-tool/planner/slab.go
@@ -16,8 +16,8 @@ import (
 	"github.com/timescale/promscale/pkg/migration-tool/utils"
 )
 
-// Block represents an in-memory storage for data that is fetched by the reader.
-type Block struct {
+// Slab represents an in-memory storage for data that is fetched by the reader.
+type Slab struct {
 	id                    int64
 	mint                  int64 // inclusive.
 	maxt                  int64 // exclusive.
@@ -30,7 +30,7 @@ type Block struct {
 	numBytesCompressed    int
 	numBytesUncompressed  int
 	pbarMux               *sync.Mutex
-	plan                  *Plan // We keep a copy of plan so that each block hsa the authority to update the stats of the planner.
+	plan                  *Plan // We keep a copy of plan so that each slab has the authority to update the stats of the planner.
 }
 
 type store struct {
@@ -41,13 +41,13 @@ type store struct {
 }
 
 // initStores initializes the stores.
-func (b *Block) initStores() {
+func (s *Slab) initStores() {
 	var (
-		proceed   = b.mint
-		increment = (b.maxt - b.mint) / int64(b.numStores)
+		proceed   = s.mint
+		increment = (s.maxt - s.mint) / int64(s.numStores)
 	)
-	for storeIndex := 0; storeIndex < b.numStores; storeIndex++ {
-		b.stores[storeIndex] = store{
+	for storeIndex := 0; storeIndex < s.numStores; storeIndex++ {
+		s.stores[storeIndex] = store{
 			id:   storeIndex,
 			mint: proceed,
 			maxt: proceed + increment,
@@ -56,22 +56,22 @@ func (b *Block) initStores() {
 	}
 	// To ensure that we cover the entire range, which may get missed due
 	// to integer division in increment, we update the maxt of the last store
-	// to be the maxt of the block.
-	b.stores[b.numStores-1].maxt = b.maxt
+	// to be the maxt of the slab.
+	s.stores[s.numStores-1].maxt = s.maxt
 }
 
 // Fetch starts fetching the samples from remote read storage based on the matchers. It takes care of concurrent pulls as well.
-func (b *Block) Fetch(ctx context.Context, client *utils.Client, mint, maxt int64, matchers []*labels.Matcher) (err error) {
+func (s *Slab) Fetch(ctx context.Context, client *utils.Client, mint, maxt int64, matchers []*labels.Matcher) (err error) {
 	var (
-		totalRequests         = b.numStores
+		totalRequests         = s.numStores
 		cancelFuncs           = make([]context.CancelFunc, totalRequests)
 		timeRangeMinutesDelta = (maxt - mint) / time.Minute.Milliseconds()
 		responseChan          = make(chan interface{}, totalRequests)
 	)
-	b.UpdatePBarMax(b.PBarMax() + totalRequests + 1)
-	b.SetDescription(fmt.Sprintf("pulling (0/%d) ...", totalRequests), 1)
+	s.UpdatePBarMax(s.PBarMax() + totalRequests + 1)
+	s.SetDescription(fmt.Sprintf("pulling (0/%d) ...", totalRequests), 1)
 	for i := 0; i < totalRequests; i++ {
-		readRequest, err := utils.CreatePrombQuery(b.stores[i].mint, b.stores[i].maxt, matchers)
+		readRequest, err := utils.CreatePrombQuery(s.stores[i].mint, s.stores[i].maxt, matchers)
 		if err != nil {
 			return fmt.Errorf("create promb query: %w", err)
 		}
@@ -84,7 +84,7 @@ func (b *Block) Fetch(ctx context.Context, client *utils.Client, mint, maxt int6
 		// idea of the data left.
 		var descriptionMsg string
 		if totalRequests == 1 {
-			descriptionMsg = fmt.Sprintf("%s | time-range: %d mins", b.pbarDescriptionPrefix, timeRangeMinutesDelta)
+			descriptionMsg = fmt.Sprintf("%s | time-range: %d mins", s.pbarDescriptionPrefix, timeRangeMinutesDelta)
 		}
 		go client.ReadChannels(cctx, readRequest, i, descriptionMsg, responseChan)
 	}
@@ -98,8 +98,8 @@ func (b *Block) Fetch(ctx context.Context, client *utils.Client, mint, maxt int6
 		case *utils.PrompbResponse:
 			bytesCompressed += response.NumBytesCompressed
 			bytesUncompressed += response.NumBytesUncompressed
-			b.timeseries = append(b.timeseries, response.Result.Timeseries...)
-			b.SetDescription(fmt.Sprintf("pulling (%d/%d) ...", i+1, totalRequests), 1)
+			s.timeseries = append(s.timeseries, response.Result.Timeseries...)
+			s.SetDescription(fmt.Sprintf("pulling (%d/%d) ...", i+1, totalRequests), 1)
 		case error:
 			for _, cancelFnc := range cancelFuncs {
 				cancelFnc()
@@ -108,78 +108,78 @@ func (b *Block) Fetch(ctx context.Context, client *utils.Client, mint, maxt int6
 		}
 	}
 	close(responseChan)
-	// We set compressed bytes in block since those are the bytes that will be pushed over the network to the write storage after snappy compression.
+	// We set compressed bytes in slab since those are the bytes that will be pushed over the network to the write storage after snappy compression.
 	// The pushed bytes are not exactly the bytesCompressed since while pushing, we add the progress metric. But,
 	// the size of progress metric along with the sample is negligible. So, it is safe to consider bytesCompressed
 	// in such a scenario.
-	b.numBytesCompressed = bytesCompressed
-	b.numBytesUncompressed = bytesUncompressed
-	b.plan.update(bytesUncompressed)
+	s.numBytesCompressed = bytesCompressed
+	s.numBytesUncompressed = bytesUncompressed
+	s.plan.update(bytesUncompressed)
 	return nil
 }
 
-// Series returns the time-series in the block.
-func (b *Block) Series() []*prompb.TimeSeries {
-	return b.timeseries
+// Series returns the time-series in the slab.
+func (s *Slab) Series() []*prompb.TimeSeries {
+	return s.timeseries
 }
 
 // GetProgressSeries returns a time-series after appending a sample to the progress-metric.
-func (b *Block) UpdateProgressSeries(ts *prompb.TimeSeries) *prompb.TimeSeries {
-	ts.Samples = []prompb.Sample{{Timestamp: b.Maxt(), Value: 1}} // One sample per block.
+func (s *Slab) UpdateProgressSeries(ts *prompb.TimeSeries) *prompb.TimeSeries {
+	ts.Samples = []prompb.Sample{{Timestamp: s.Maxt(), Value: 1}} // One sample per slab.
 	return ts
 }
 
-func (b *Block) UpdatePBarMax(steps int) {
-	if b.pbarDescriptionPrefix == "" {
+func (s *Slab) UpdatePBarMax(steps int) {
+	if s.pbarDescriptionPrefix == "" {
 		return
 	}
-	b.pbar.ChangeMax(steps)
+	s.pbar.ChangeMax(steps)
 }
 
-func (b *Block) PBarMax() int {
-	if b.pbarDescriptionPrefix == "" {
+func (s *Slab) PBarMax() int {
+	if s.pbarDescriptionPrefix == "" {
 		return -1
 	}
-	return b.pbar.GetMax()
+	return s.pbar.GetMax()
 }
 
-func (b *Block) SetDescription(description string, proceed int) {
-	if b.pbarDescriptionPrefix == "" {
+func (s *Slab) SetDescription(description string, proceed int) {
+	if s.pbarDescriptionPrefix == "" {
 		return
 	}
-	b.pbarMux.Lock()
-	defer b.pbarMux.Unlock()
-	_ = b.pbar.Add(proceed)
-	b.pbar.Describe(fmt.Sprintf("%s | %s", b.pbarDescriptionPrefix, description))
+	s.pbarMux.Lock()
+	defer s.pbarMux.Unlock()
+	_ = s.pbar.Add(proceed)
+	s.pbar.Describe(fmt.Sprintf("%s | %s", s.pbarDescriptionPrefix, description))
 }
 
 // Done updates the text and sets the spinner to done.
-func (b *Block) Done() error {
-	if b.pbarDescriptionPrefix == "" {
+func (s *Slab) Done() error {
+	if s.pbarDescriptionPrefix == "" {
 		return nil
 	}
-	b.SetDescription(
-		fmt.Sprintf("pushed %.2f MB. Memory footprint: %.2f MB.", float64(b.numBytesCompressed)/float64(utils.Megabyte), float64(b.numBytesUncompressed)/float64(utils.Megabyte)),
+	s.SetDescription(
+		fmt.Sprintf("pushed %.2f MB. Memory footprint: %.2f MB.", float64(s.numBytesCompressed)/float64(utils.Megabyte), float64(s.numBytesUncompressed)/float64(utils.Megabyte)),
 		1,
 	)
-	b.done = true
-	if err := b.pbar.Finish(); err != nil {
-		return fmt.Errorf("finish block-lifecycle: %w", err)
+	s.done = true
+	if err := s.pbar.Finish(); err != nil {
+		return fmt.Errorf("finish slab-lifecycle: %w", err)
 	}
 	return nil
 }
 
-// IsEmpty returns true if the block does not contain any time-series.
-func (b *Block) IsEmpty() bool {
-	return len(b.timeseries) == 0
+// IsEmpty returns true if the slab does not contain any time-series.
+func (s *Slab) IsEmpty() bool {
+	return len(s.timeseries) == 0
 }
 
-// Mint returns the mint of the block (inclusive).
-func (b *Block) Mint() int64 {
-	return b.mint
+// Mint returns the mint of the slab (inclusive).
+func (s *Slab) Mint() int64 {
+	return s.mint
 }
 
-// Maxt returns the maxt of the block (exclusive).
-func (b *Block) Maxt() int64 {
-	return b.maxt
+// Maxt returns the maxt of the slab (exclusive).
+func (s *Slab) Maxt() int64 {
+	return s.maxt
 }
