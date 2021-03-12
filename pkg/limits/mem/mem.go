f8bc4792fee0b20d305774bdfc610be35b8ca6df
Matvey Arye <mat@timescale.com>
2021-03-12 16:33:41 -0500

    Add utility library to get the total system memory.
    Rely on github.com/pbnjay/memory for most platforms
    butt in linux try to find the cgroup limits first
    so that we are friendly to docker and k8s.
    
    Tested only in docker and MacOS so far.

---
+++ pkg/limits/mem/mem.go
@@ -0,0 +1,15 @@
+// +build !linux
+
+// This file and its contents are licensed under the Apache License 2.0.
+// Please see the included NOTICE for copyright information and
+// LICENSE for a copy of the license
+
+package mem
+
+import (
+	"github.com/pbnjay/memory"
+)
+
+func SystemMemory() uint64 {
+	return memory.TotalMemory()
+}
