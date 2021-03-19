92b9495737e3a164e79de59b7a93bcaa55bc62cf
Matvey Arye <mat@timescale.com>
2021-03-19 13:40:16 -0400

    Adjust sql update scripts

--- pkg/migrations/sql/idempotent/ha.sql
+++ pkg/migrations/sql/idempotent/ha.sql
@@ -1,21 +1,3 @@
-CREATE TABLE SCHEMA_CATALOG.ha_leases
-(
-    cluster_name TEXT PRIMARY KEY,
-    leader_name  TEXT,
-    lease_start  TIMESTAMPTZ,
-    lease_until  TIMESTAMPTZ
-);
-
-CREATE TABLE SCHEMA_CATALOG.ha_leases_logs
-(
-    cluster_name TEXT        NOT NULL,
-    leader_name  TEXT        NOT NULL,
-    lease_start  TIMESTAMPTZ NOT NULL, -- inclusive
-    lease_until  TIMESTAMPTZ,          -- exclusive
-    PRIMARY KEY (cluster_name, leader_name, lease_start)
-);
-
-
 -- function that trigger to automatically keep the log calls
 CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.ha_leases_audit_fn()
     RETURNS TRIGGER
@@ -44,19 +26,6 @@ BEGIN
     RETURN NEW;
 END;
 $func$ LANGUAGE plpgsql VOLATILE;
-
--- trigger to automatically keep the log
-CREATE TRIGGER ha_leases_audit
-    AFTER INSERT OR UPDATE
-    ON SCHEMA_CATALOG.ha_leases
-    FOR EACH ROW
-EXECUTE PROCEDURE SCHEMA_CATALOG.ha_leases_audit_fn();
-
--- default values for lease
-INSERT INTO SCHEMA_CATALOG.default(key, value)
-VALUES ('ha_lease_timeout', '1m'),
-       ('ha_lease_refresh', '10s')
-ON CONFLICT (key) DO UPDATE SET value = EXCLUDED.value;
 
 -- ha api functions
 CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.update_lease(cluster TEXT, writer TEXT, min_time TIMESTAMPTZ,
---
+++ pkg/migrations/sql/versions/dev/0.2.2-dev/1-set_up_ha.sql
@@ -0,0 +1,41 @@
+CREATE TABLE SCHEMA_CATALOG.ha_leases
+(
+    cluster_name TEXT PRIMARY KEY,
+    leader_name  TEXT,
+    lease_start  TIMESTAMPTZ,
+    lease_until  TIMESTAMPTZ
+);
+
+CREATE TABLE SCHEMA_CATALOG.ha_leases_logs
+(
+    cluster_name TEXT        NOT NULL,
+    leader_name  TEXT        NOT NULL,
+    lease_start  TIMESTAMPTZ NOT NULL, -- inclusive
+    lease_until  TIMESTAMPTZ,          -- exclusive
+    PRIMARY KEY (cluster_name, leader_name, lease_start)
+);
+
+
+-- STUB for function that trigger to automatically keep the log calls - real implementation in ha.sql
+CREATE OR REPLACE FUNCTION SCHEMA_CATALOG.ha_leases_audit_fn()
+    RETURNS TRIGGER
+AS
+$func$
+BEGIN
+    RAISE 'Just a stub, should be overwritten'
+    RETURN NEW;
+END;
+$func$ LANGUAGE plpgsql VOLATILE;
+
+-- trigger to automatically keep the log
+CREATE TRIGGER ha_leases_audit
+    AFTER INSERT OR UPDATE
+    ON SCHEMA_CATALOG.ha_leases
+    FOR EACH ROW
+EXECUTE PROCEDURE SCHEMA_CATALOG.ha_leases_audit_fn();
+
+-- default values for lease
+INSERT INTO SCHEMA_CATALOG.default(key, value)
+VALUES ('ha_lease_timeout', '1m'),
+       ('ha_lease_refresh', '10s')
+ON CONFLICT (key) DO UPDATE SET value = EXCLUDED.value;
