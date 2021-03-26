4098751296d14578df8bc85bcd04f5db25e65418
Matvey Arye <mat@timescale.com>
2021-03-25 22:46:58 -0400

    Add support function test

---
+++ pkg/tests/testdata/sql/support.sql
@@ -0,0 +1,23 @@
+\set ECHO all
+\set ON_ERROR_STOP 1
+
+SELECT _prom_catalog.get_or_create_metric_table_name('cpu_usage');
+SELECT _prom_catalog.get_or_create_metric_table_name('cpu_total');
+CALL _prom_catalog.finalize_metric_creation();
+INSERT INTO prom_data.cpu_usage
+SELECT timestamptz '2000-01-01 02:03:04'+(interval '1s' * g), 100.1 + g, _prom_catalog.get_or_create_series_id('{"__name__": "cpu_usage", "namespace":"dev", "node": "brain"}')
+FROM generate_series(1,10) g;
+INSERT INTO prom_data.cpu_usage
+SELECT timestamptz '2000-01-01 02:03:04'+(interval '1s' * g), 100.1 + g, _prom_catalog.get_or_create_series_id('{"__name__": "cpu_usage", "namespace":"production", "node": "pinky", "new_tag":"foo"}')
+FROM generate_series(1,10) g;
+INSERT INTO prom_data.cpu_total
+SELECT timestamptz '2000-01-01 02:03:04'+(interval '1s' * g), 100.0, _prom_catalog.get_or_create_series_id('{"__name__": "cpu_total", "namespace":"dev", "node": "brain"}')
+FROM generate_series(1,10) g;
+INSERT INTO prom_data.cpu_total
+SELECT timestamptz '2000-01-01 02:03:04'+(interval '1s' * g), 100.0, _prom_catalog.get_or_create_series_id('{"__name__": "cpu_total", "namespace":"production", "node": "pinky", "new_tag_2":"bar"}')
+FROM generate_series(1,10) g;
+
+--this should use a subquery with the Promscale extension but not without
+--this is thanks to the support function make_call_subquery_support
+ANALYZE;
+EXPLAIN (costs off) SELECT time, value, jsonb(labels), val(namespace_id) FROM cpu_usage WHERE labels ? ('namespace' !== 'dev' ) ORDER BY time, series_id LIMIT 5;
