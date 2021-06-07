4483827a4c573a26203e60dcf35f015eaef57124
Harkishen Singh <harkishensingh@hotmail.com>
2021-06-07 14:44:51 +0530

    Update Prompb with upstream upto May 7.
    Signed-off-by: Harkishen Singh <harkishensingh@hotmail.com>
    
    This commit updates the Prompb with Upstream including all changes
    till May 7. This includes changes done till commit
    8fd73b1d281d3227c337b291e8c797a85251d3d5.

--- pkg/prompb/custom.ts.go
+++ pkg/prompb/custom.ts.go
@@ -5,5 +5,6 @@ func (m *WriteRequest) Reset() {
 	*m = WriteRequest{Timeseries: m.Timeseries[:0], Metadata: m.Metadata[:0]}
 }
 func (m *TimeSeries) Reset() {
-	*m = TimeSeries{Labels: m.Labels[:0], Samples: m.Samples[:0]}
+	*m = TimeSeries{Labels: m.Labels[:0], Exemplars: m.Exemplars[:0], Samples: m.Samples[:0]}
 }
+func (m *Exemplar) Reset() { *m = Exemplar{Labels: m.Labels[:0]} }
