timescale/timescale-prometheus

Update metric cache to respect exemplar table entries.
Signed-off-by: Harkishen Singh <harkishensingh@hotmail.com>

pkg/pgclient/client.go
@@ -139,13 +139,12 @@ func NewClientWithPool(cfg *Config, numCopiers int, dbConn pgxconn.PgxConn, mt t
 		IgnoreCompressedChunks: cfg.IgnoreCompressedChunks,
 	}
 
-	var dbIngestor *ingestor.DBIngestor
+	var (
+		dbIngestor          *ingestor.DBIngestor
+		exemplarKeyPosCache = cache.NewExemplarLabelsPosCache(cfg.CacheConfig)
+	)
 	if !readOnly {
-		var (
-			err error
-
-			exemplarKeyPosCache = cache.NewExemplarLabelsPosCache(cfg.CacheConfig)
-		)
+		var err error
 		dbIngestor, err = ingestor.NewPgxIngestor(dbConn, metricsCache, seriesCache, exemplarKeyPosCache, &c)
 		if err != nil {
 			log.Error("msg", "err starting the ingestor", "err", err)
@@ -154,7 +153,7 @@ func NewClientWithPool(cfg *Config, numCopiers int, dbConn pgxconn.PgxConn, mt t
 	}
 
 	labelsReader := lreader.NewLabelsReader(dbConn, labelsCache)
-	dbQuerier := querier.NewQuerier(dbConn, metricsCache, labelsReader, mt.ReadAuthorizer())
+	dbQuerier := querier.NewQuerier(dbConn, metricsCache, labelsReader, exemplarKeyPosCache, mt.ReadAuthorizer())
 	queryable := query.NewQueryable(dbQuerier, labelsReader)
 
 	healthChecker := health.NewHealthChecker(dbConn)

timescale/promscale

pkg/pgclient/client.go
@@ -137,13 +137,12 @@ func NewClientWithPool(cfg *Config, numCopiers int, connPool *pgxpool.Pool, mt t
 		IgnoreCompressedChunks: cfg.IgnoreCompressedChunks,
 	}
 
-	var dbIngestor *ingestor.DBIngestor
+	var (
+		dbIngestor          *ingestor.DBIngestor
+		exemplarKeyPosCache = cache.NewExemplarLabelsPosCache(cfg.CacheConfig)
+	)
 	if !readOnly {
-		var (
-			err error
-
-			exemplarKeyPosCache = cache.NewExemplarLabelsPosCache(cfg.CacheConfig)
-		)
+		var err error
 		dbIngestor, err = ingestor.NewPgxIngestor(dbConn, metricsCache, seriesCache, exemplarKeyPosCache, &c)
 		if err != nil {
 			log.Error("msg", "err starting the ingestor", "err", err)
@@ -154,7 +153,7 @@ func NewClientWithPool(cfg *Config, numCopiers int, connPool *pgxpool.Pool, mt t
 	labelsReader := lreader.NewLabelsReader(dbConn, labelsCache)
 
 	dbQuerierConn := pgxconn.NewQueryLoggingPgxConn(connPool)
-	dbQuerier := querier.NewQuerier(dbQuerierConn, metricsCache, labelsReader, mt.ReadAuthorizer())
+	dbQuerier := querier.NewQuerier(dbQuerierConn, metricsCache, labelsReader, exemplarKeyPosCache, mt.ReadAuthorizer())
 	queryable := query.NewQueryable(dbQuerier, labelsReader)
 
 	healthChecker := health.NewHealthChecker(dbConn)