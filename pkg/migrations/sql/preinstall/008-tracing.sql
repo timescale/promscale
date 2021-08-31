cae8bae682bc9f5e704f88217531d133d193f0e9
John Pruitt <jgpruitt@gmail.com>
2021-08-31 14:24:51 -0500

    operators working for attribute filtering

--- pkg/migrations/sql/preinstall/008-tracing.sql
+++ pkg/migrations/sql/preinstall/008-tracing.sql
@@ -13,6 +13,9 @@ DEFAULT '{}'::jsonb
 CHECK (jsonb_typeof(value) = 'object');
 GRANT USAGE ON DOMAIN SCHEMA_TRACING.attribute_map TO prom_reader;
 
+CREATE DOMAIN SCHEMA_TRACING.attribute_matchers AS SCHEMA_TRACING.attribute_map[] NOT NULL;
+GRANT USAGE ON DOMAIN SCHEMA_TRACING.attribute_matchers TO prom_reader;
+
 CREATE DOMAIN SCHEMA_TRACING.attribute_type smallint NOT NULL;
 GRANT USAGE ON DOMAIN SCHEMA_TRACING.attribute_type TO prom_reader;
 
@@ -35,7 +38,7 @@ CREATE TABLE SCHEMA_TRACING.attribute
     key text NOT NULL,
     value jsonb,
     FOREIGN KEY (key) REFERENCES SCHEMA_TRACING.attribute_key (key) ON DELETE CASCADE,
-    UNIQUE (key, value) INCLUDE (id, key_id, attribute_type)
+    UNIQUE (key, value) INCLUDE (attribute_type)
 )
 PARTITION BY HASH (key);
 GRANT SELECT ON TABLE SCHEMA_TRACING.attribute TO prom_reader;
@@ -131,15 +134,14 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE SCHEMA_TRACING.trace TO prom_write
 
 CREATE TABLE IF NOT EXISTS SCHEMA_TRACING.span
 (
-    trace_id SCHEMA_TRACING.trace_id NOT NULL /*REFERENCES SCHEMA_TRACING.trace(id)*/,
+    trace_id SCHEMA_TRACING.trace_id NOT NULL,
     span_id bigint NOT NULL,
     trace_state text,
     parent_span_id bigint NULL,
-    name_id bigint NOT NULL /*REFERENCES SCHEMA_TRACING.span_name (id)*/,
+    name_id bigint NOT NULL,
     span_kind SCHEMA_TRACING.span_kind,
     start_time timestamptz NOT NULL,
     end_time timestamptz NOT NULL,
---    duration interval NOT NULL GENERATED ALWAYS AS ( end_time - start_time ) STORED,
     span_attributes SCHEMA_TRACING.attribute_map,
     dropped_attributes_count int NOT NULL default 0,
     event_time tstzrange NOT NULL default tstzrange('infinity', 'infinity', '()'),
@@ -147,17 +149,14 @@ CREATE TABLE IF NOT EXISTS SCHEMA_TRACING.span
     dropped_link_count int NOT NULL default 0,
     status_code SCHEMA_TRACING.status_code,
     status_message text,
-    instrumentation_library_id bigint /*REFERENCES SCHEMA_TRACING.instrumentation_library (id)*/,
+    instrumentation_library_id bigint,
     resource_attributes SCHEMA_TRACING.attribute_map,
     resource_dropped_attributes_count int NOT NULL default 0,
-    resource_schema_url_id BIGINT NOT NULL /*REFERENCES SCHEMA_TRACING.schema_url(id)*/,
+    resource_schema_url_id BIGINT NOT NULL,
     PRIMARY KEY (span_id, trace_id, start_time),
     CHECK (start_time <= end_time)
 );
 CREATE INDEX ON SCHEMA_TRACING.span USING BTREE (trace_id, parent_span_id);
---CREATE INDEX ON SCHEMA_TRACING.span USING BTREE (span_id);
---CREATE INDEX ON SCHEMA_TRACING.span USING BTREE (name_id);
---CREATE INDEX ON SCHEMA_TRACING.span USING GIST (tstzrange(start_time, end_time, '[]'));
 CREATE INDEX ON SCHEMA_TRACING.span USING GIN (span_attributes jsonb_path_ops);
 CREATE INDEX ON SCHEMA_TRACING.span USING GIN (resource_attributes jsonb_path_ops);
 SELECT create_hypertable('SCHEMA_TRACING.span', 'start_time', partitioning_column=>'trace_id', number_partitions=>1);
@@ -173,7 +172,6 @@ CREATE TABLE IF NOT EXISTS SCHEMA_TRACING.event
     name text NOT NULL CHECK (name != ''),
     attributes SCHEMA_TRACING.attribute_map,
     dropped_attributes_count int NOT NULL DEFAULT 0
-    --FOREIGN KEY (span_id, trace_id) REFERENCES SCHEMA_TRACING.span (span_id, trace_id) ON DELETE CASCADE -- foreign keys to hypertables are not supported
 );
 CREATE INDEX ON SCHEMA_TRACING.event USING GIN (attributes jsonb_path_ops);
 CREATE INDEX ON SCHEMA_TRACING.event USING BTREE (span_id, time) INCLUDE (trace_id);
@@ -193,10 +191,159 @@ CREATE TABLE IF NOT EXISTS SCHEMA_TRACING.link
     attributes SCHEMA_TRACING.attribute_map,
     dropped_attributes_count int NOT NULL DEFAULT 0,
     event_number smallint NOT NULL
-    -- FOREIGN KEY (span_id, trace_id) REFERENCES SCHEMA_TRACING.span (span_id, trace_id) ON DELETE CASCADE -- foreign keys to hypertables are not supported
 );
 CREATE INDEX ON SCHEMA_TRACING.link USING BTREE (span_id, span_start_time) INCLUDE (trace_id);
 CREATE INDEX ON SCHEMA_TRACING.link USING GIN (attributes jsonb_path_ops);
 SELECT create_hypertable('SCHEMA_TRACING.link', 'span_start_time', partitioning_column=>'trace_id', number_partitions=>1);
 GRANT SELECT ON TABLE SCHEMA_TRACING.link TO prom_reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE SCHEMA_TRACING.link TO prom_writer;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_jsonpath(_key text, _qry jsonpath)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $sql$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$sql$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_text_equal(_key text, _val text)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_text_not_equal(_key text, _val text)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_int_equal(_key text, _val int)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_int_not_equal(_key text, _val int)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_bool_equal(_key text, _val boolean)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_bool_not_equal(_key text, _val boolean)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_regex(_key text,  _pattern text)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.get_attributes_not_regex(_key text,  _pattern text)
+RETURNS SCHEMA_TRACING.attribute_matchers
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT '{}'::SCHEMA_TRACING.attribute_matchers
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OR REPLACE FUNCTION SCHEMA_TRACING.match_attribute_map(_attr_map SCHEMA_TRACING.attribute_map, _matchers SCHEMA_TRACING.attribute_matchers)
+RETURNS boolean
+AS $func$
+    -- this function body will be replaced later in idempotent script
+    -- it's only here so we can create the operators (no "if not exists" for operators)
+    SELECT false
+$func$
+LANGUAGE SQL STABLE PARALLEL SAFE STRICT;
+
+CREATE OPERATOR SCHEMA_TRACING.@? (
+    LEFTARG = text,
+    RIGHTARG = jsonpath,
+    FUNCTION = SCHEMA_TRACING.get_attributes_jsonpath
+);
+
+CREATE OPERATOR SCHEMA_TRACING.== (
+    LEFTARG = text,
+    RIGHTARG = text,
+    FUNCTION = SCHEMA_TRACING.get_attributes_text_equal
+);
+
+CREATE OPERATOR SCHEMA_TRACING.!== (
+    LEFTARG = text,
+    RIGHTARG = text,
+    FUNCTION = SCHEMA_TRACING.get_attributes_text_not_equal
+);
+
+CREATE OPERATOR SCHEMA_TRACING.== (
+    LEFTARG = text,
+    RIGHTARG = int,
+    FUNCTION = SCHEMA_TRACING.get_attributes_int_equal
+);
+
+CREATE OPERATOR SCHEMA_TRACING.!== (
+    LEFTARG = text,
+    RIGHTARG = int,
+    FUNCTION = SCHEMA_TRACING.get_attributes_int_not_equal
+);
+
+CREATE OPERATOR SCHEMA_TRACING.== (
+    LEFTARG = text,
+    RIGHTARG = boolean,
+    FUNCTION = SCHEMA_TRACING.get_attributes_bool_equal
+);
+
+CREATE OPERATOR SCHEMA_TRACING.!== (
+    LEFTARG = text,
+    RIGHTARG = boolean,
+    FUNCTION = SCHEMA_TRACING.get_attributes_bool_not_equal
+);
+
+CREATE OPERATOR SCHEMA_TRACING.==~ (
+    LEFTARG = text,
+    RIGHTARG = text,
+    FUNCTION = SCHEMA_TRACING.get_attributes_regex
+);
+
+CREATE OPERATOR SCHEMA_TRACING.!=~ (
+    LEFTARG = text,
+    RIGHTARG = text,
+    FUNCTION = SCHEMA_TRACING.get_attributes_not_regex
+);
+
+CREATE OPERATOR SCHEMA_TRACING.? (
+    LEFTARG = SCHEMA_TRACING.attribute_map,
+    RIGHTARG = SCHEMA_TRACING.attribute_matchers,
+    FUNCTION = SCHEMA_TRACING.match_attribute_map
+);
