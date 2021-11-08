7507e0c0e417bf78e64b708a7a205f2701f8ad90
Ante Kresic <ante.kresic@gmail.com>
2021-11-08 13:58:15 +0100

    Remove deprecated go-kit packages and use current ones

--- pkg/query/query_engine.go
+++ pkg/query/query_engine.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/timescale/promscale/pkg/promql"
 )
