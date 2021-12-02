eb00b72100f9dc37d8890b38ead46872b0e4803b
James Guthrie <jguthrie@timescale.com>
2021-12-02 07:50:08 +0100

    Prepare for the 0.7.0 release

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -38,9 +38,9 @@ var (
 	// It is customary to bump the version by incrementing the numeral after
 	// the `dev` tag. The SQL migration script name must correspond to the /new/ version.
 
-	Promscale                           = "0.7.0-beta.1.dev.5"
+	Promscale                           = "0.7.0"
 	PrevReleaseVersion                  = "0.7.0-beta.1"
-	PromMigrator                        = "0.0.2"
+	PromMigrator                        = "0.0.3"
 	CommitHash                          = ""
 	EarliestUpgradeTestVersion          = "0.1.0"
 	EarliestUpgradeTestVersionMultinode = "0.1.4" //0.1.4 earliest version that supports tsdb 2.0
