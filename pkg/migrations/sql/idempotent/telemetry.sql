a6808962160c12a4bb3f339dade92c4d6f5add4b
Harkishen-Singh <harkishensingh@hotmail.com>
2021-12-08 14:50:05 +0530

    SQL objects for telemetry-housekeeper.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>
    
    This commit implements the following:
    1. Insert counter reset row (files: 012-telemetry.sql,
    2-telemetry_housekeeper.sql)
    2. Function that increments the counter reset row with values of stale
    Promscale instances followed by deleting them (files: telemetry.sql)

---
+++ pkg/migrations/sql/idempotent/telemetry.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE FUNCTION SCHEMA_PS_CATALOG.clean_stale_promscales_after_counter_reset()
+RETURNS VOID AS
+$$
+    BEGIN
+        -- Check if any stale row is present.
+        IF (SELECT count(*) = 0 FROM SCHEMA_PS_CATALOG.promscale_instance_information
+                WHERE
+                      is_counter_reset_row = FALSE
+                  AND
+                      current_timestamp - last_updated > INTERVAL '1 HOUR') THEN
+            RETURN;
+        END IF;
+
+        -- Stale rows are present. Update the counter reset row.
+        UPDATE SCHEMA_PS_CATALOG.promscale_instance_information SET
+            promscale_ingested_samples_total                    = subquery.counter_reset_metrics_ingested_samples + subquery.stale_metrics_ingested_samples,
+            promscale_metrics_queries_executed_total            = subquery.counter_reset_metrics_queries_executed + subquery.stale_metrics_queries_executed,
+            promscale_metrics_queries_timedout_total            = subquery.counter_reset_metrics_queries_timed_out + subquery.stale_metrics_queries_timed_out,
+            promscale_metrics_queries_failed_total              = subquery.counter_reset_metrics_queries_failed + subquery.stale_metrics_queries_failed,
+            promscale_trace_query_requests_executed_total       = subquery.counter_reset_traces_queries_executed + subquery.stale_traces_queries_executed,
+            promscale_trace_dependency_requests_executed_total  = subquery.counter_reset_traces_dependency_queries_executed + subquery.stale_traces_dependency_queries_executed,
+            last_updated = current_timestamp
+        FROM
+            (
+                -- Basically, we are collecting all stale rows under `stale.` and the actual counter-reset row under `counter_reset.`
+                -- and the are aggregated in the above SET step. We can aggregate it here, but its more cleaner to do in SET.
+                SELECT
+                    sum(stale.promscale_ingested_samples_total)  			        stale_metrics_ingested_samples,
+                    sum(stale.promscale_metrics_queries_executed_total)  		    stale_metrics_queries_executed,
+                    sum(stale.promscale_metrics_queries_timedout_total)  			stale_metrics_queries_timed_out,
+                    sum(stale.promscale_metrics_queries_failed_total)  				stale_metrics_queries_failed,
+                    sum(stale.promscale_trace_query_requests_executed_total)        stale_traces_queries_executed,
+                    sum(stale.promscale_trace_dependency_requests_executed_total)  	stale_traces_dependency_queries_executed,
+
+                    counter_reset.promscale_ingested_samples_total  			        counter_reset_metrics_ingested_samples,
+                    counter_reset.promscale_metrics_queries_executed_total 			    counter_reset_metrics_queries_executed,
+                    counter_reset.promscale_metrics_queries_timedout_total  			counter_reset_metrics_queries_timed_out,
+                    counter_reset.promscale_metrics_queries_failed_total  			    counter_reset_metrics_queries_failed,
+                    counter_reset.promscale_trace_query_requests_executed_total  	    counter_reset_traces_queries_executed,
+                    counter_reset.promscale_trace_dependency_requests_executed_total    counter_reset_traces_dependency_queries_executed
+                FROM
+                    SCHEMA_PS_CATALOG.promscale_instance_information stale INNER JOIN SCHEMA_PS_CATALOG.promscale_instance_information counter_reset ON true
+                WHERE
+                    counter_reset.is_counter_reset_row = TRUE AND stale.is_counter_reset_row = FALSE
+                GROUP BY
+                    counter_reset.promscale_ingested_samples_total,
+                    counter_reset.promscale_metrics_queries_executed_total,
+                    counter_reset.promscale_metrics_queries_timedout_total,
+                    counter_reset.promscale_metrics_queries_failed_total,
+                    counter_reset.promscale_trace_query_requests_executed_total,
+                    counter_reset.promscale_trace_dependency_requests_executed_total
+            ) AS subquery
+        WHERE deletable = false AND current_timestamp - last_updated > interval '1 HOUR';
+
+        -- Delete the stale rows.
+        DELETE FROM SCHEMA_PS_CATALOG.promscale_instance_information WHERE deletable = TRUE AND current_timestamp - last_updated > interval '1 HOUR';
+    END;
+$$
+LANGUAGE PLPGSQL;
--- pkg/migrations/sql/preinstall/012-telemetry.sql
+++ pkg/migrations/sql/preinstall/012-telemetry.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS SCHEMA_PS_CATALOG.promscale_instance_information (
 );
 GRANT SELECT ON TABLE SCHEMA_PS_CATALOG.promscale_instance_information TO prom_reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE SCHEMA_PS_CATALOG.promscale_instance_information TO prom_writer;
+
+-- Write a counter reset row, i.e., the first row in the table. Purpose:
+-- The above promscale_.* rows logically behave as counter. They get deleted by
+-- telemetry-housekeeper promscale when last_updated is too old to be stale. Since
+-- counters are always increasing, if these rows get deleted, it will result in data-loss.
+-- To avoid this loss of data, we treat the first row as immutable, and use it for incrementing
+-- the attributes of this row, with the values of the stale rows before they are deleted.
+INSERT INTO SCHEMA_PS_CATALOG.promscale_instance_information (uuid, last_updated, is_counter_reset_row)
+    VALUES ('00000000-0000-0000-0000-000000000000', current_timestamp, TRUE);
