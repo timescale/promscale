f026649922dbf61b814a3c56972f3cac8ef8f3e1
Harkishen-Singh <harkishensingh@hotmail.com>
2021-12-10 17:05:15 +0530

    Allow all Promscale to be houeskeepers based on time.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>
    
    This commits remmoves the concept of only 1 housekeeper, meaning all
    Promscales can do housekeeping depending on the time it was done last.
    If the last housekeeping was done an hour ago, do the housekeeping now,
    otherwise do not do it.
    
    IMPORTANT: The state of last done housekeeping time is stored as the last_update
    attribute of the counter_reset row.

--- pkg/migrations/sql/idempotent/telemetry.sql
+++ pkg/migrations/sql/idempotent/telemetry.sql
@@ -1,59 +1,62 @@
-CREATE OR REPLACE FUNCTION SCHEMA_PS_CATALOG.clean_stale_promscales_after_counter_reset()
-RETURNS VOID AS
+-- promscale_telemetry_housekeeping() does telemetry housekeeping stuff, which includes
+-- searching the table for telemetry_sync_duration, looking for stale promscales, if found,
+-- adding their values into the counter_reset row, and then cleaning up the stale
+-- promscale instances.
+-- It is concurrency safe, since it takes lock on the promscale_instance_information table,
+-- making sure that at one time, only one housekeeping is being done.
+-- 
+-- It returns TRUE if last run was beyond telemetry_sync_duration, otherwise FALSE.
+CREATE OR REPLACE FUNCTION SCHEMA_PS_CATALOG.promscale_telemetry_housekeeping(telemetry_sync_duration INTERVAL DEFAULT INTERVAL '1 HOUR')
+RETURNS BOOLEAN AS
 $$
+    DECLARE
+        should_update_telemetry BOOLEAN;
     BEGIN
-        -- Check if any stale row is present.
-        IF (SELECT count(*) = 0 FROM SCHEMA_PS_CATALOG.promscale_instance_information
-                WHERE
-                      is_counter_reset_row = FALSE
-                  AND
-                      current_timestamp - last_updated > INTERVAL '1 HOUR') THEN
-            RETURN;
+        BEGIN
+            LOCK TABLE SCHEMA_PS_CATALOG.promscale_instance_information IN ACCESS EXCLUSIVE MODE NOWAIT; -- Do not wait for the lock as some promscale is already cleaning up the stuff.
+        EXCEPTION
+            WHEN SQLSTATE '55P03' THEN
+                RETURN FALSE;
+        END;
+
+        -- This guarantees that we update our telemetry once every telemetry_sync_duration.
+        SELECT count(*) = 0 INTO should_update_telemetry FROM SCHEMA_PS_CATALOG.promscale_instance_information
+            WHERE is_counter_reset_row = TRUE AND current_timestamp - last_updated < telemetry_sync_duration;
+
+        IF NOT should_update_telemetry THEN
+            -- Some Promscale did the housekeeping work within the expected interval. Hence, nothing to do, so exit.
+            RETURN FALSE;
         END IF;
 
-        -- Stale rows are present. Update the counter reset row.
+
+        WITH deleted_rows AS (
+            DELETE FROM SCHEMA_PS_CATALOG.promscale_instance_information 
+            WHERE is_counter_reset_row = FALSE AND current_timestamp - last_updated > (telemetry_sync_duration * 2) -- consider adding stats of deleted rows to persist counter reset behaviour.
+            RETURNING *
+        )
         UPDATE SCHEMA_PS_CATALOG.promscale_instance_information SET
-            promscale_ingested_samples_total                    = subquery.counter_reset_metrics_ingested_samples + subquery.stale_metrics_ingested_samples,
-            promscale_metrics_queries_executed_total            = subquery.counter_reset_metrics_queries_executed + subquery.stale_metrics_queries_executed,
-            promscale_metrics_queries_timedout_total            = subquery.counter_reset_metrics_queries_timed_out + subquery.stale_metrics_queries_timed_out,
-            promscale_metrics_queries_failed_total              = subquery.counter_reset_metrics_queries_failed + subquery.stale_metrics_queries_failed,
-            promscale_trace_query_requests_executed_total       = subquery.counter_reset_traces_queries_executed + subquery.stale_traces_queries_executed,
-            promscale_trace_dependency_requests_executed_total  = subquery.counter_reset_traces_dependency_queries_executed + subquery.stale_traces_dependency_queries_executed,
+            promscale_ingested_samples_total                    = promscale_ingested_samples_total + COALESCE(x.del_promscale_ingested_samples_total, 0),
+            promscale_metrics_queries_executed_total            = promscale_metrics_queries_executed_total + COALESCE(x.del_promscale_metrics_queries_executed_total, 0),
+            promscale_metrics_queries_timedout_total            = promscale_metrics_queries_timedout_total + COALESCE(x.del_promscale_metrics_queries_timedout_total, 0),
+            promscale_metrics_queries_failed_total              = promscale_metrics_queries_failed_total + COALESCE(x.del_promscale_metrics_queries_failed_total, 0),
+            promscale_trace_query_requests_executed_total       = promscale_trace_query_requests_executed_total + COALESCE(x.del_promscale_trace_query_requests_executed_total, 0),
+            promscale_trace_dependency_requests_executed_total  = promscale_trace_dependency_requests_executed_total + COALESCE(x.del_promscale_trace_dependency_requests_executed_total, 0),
             last_updated = current_timestamp
         FROM
-            (
-                -- Basically, we are collecting all stale rows under `stale.` and the actual counter-reset row under `counter_reset.`
-                -- and the are aggregated in the above SET step. We can aggregate it here, but its more cleaner to do in SET.
-                SELECT
-                    sum(stale.promscale_ingested_samples_total)  			        stale_metrics_ingested_samples,
-                    sum(stale.promscale_metrics_queries_executed_total)  		    stale_metrics_queries_executed,
-                    sum(stale.promscale_metrics_queries_timedout_total)  			stale_metrics_queries_timed_out,
-                    sum(stale.promscale_metrics_queries_failed_total)  				stale_metrics_queries_failed,
-                    sum(stale.promscale_trace_query_requests_executed_total)        stale_traces_queries_executed,
-                    sum(stale.promscale_trace_dependency_requests_executed_total)  	stale_traces_dependency_queries_executed,
-
-                    counter_reset.promscale_ingested_samples_total  			        counter_reset_metrics_ingested_samples,
-                    counter_reset.promscale_metrics_queries_executed_total 			    counter_reset_metrics_queries_executed,
-                    counter_reset.promscale_metrics_queries_timedout_total  			counter_reset_metrics_queries_timed_out,
-                    counter_reset.promscale_metrics_queries_failed_total  			    counter_reset_metrics_queries_failed,
-                    counter_reset.promscale_trace_query_requests_executed_total  	    counter_reset_traces_queries_executed,
-                    counter_reset.promscale_trace_dependency_requests_executed_total    counter_reset_traces_dependency_queries_executed
-                FROM
-                    SCHEMA_PS_CATALOG.promscale_instance_information stale INNER JOIN SCHEMA_PS_CATALOG.promscale_instance_information counter_reset ON true
-                WHERE 
-                    counter_reset.is_counter_reset_row = TRUE AND (stale.is_counter_reset_row = FALSE AND current_timestamp - stale.last_updated > interval '1 HOUR') -- consider only those rows as stale who were last updated beyond an hour.
-                GROUP BY
-                    counter_reset.promscale_ingested_samples_total,
-                    counter_reset.promscale_metrics_queries_executed_total,
-                    counter_reset.promscale_metrics_queries_timedout_total,
-                    counter_reset.promscale_metrics_queries_failed_total,
-                    counter_reset.promscale_trace_query_requests_executed_total,
-                    counter_reset.promscale_trace_dependency_requests_executed_total
-            ) AS subquery
+        (
+            SELECT
+                sum(promscale_ingested_samples_total)  			        as del_promscale_ingested_samples_total,
+                sum(promscale_metrics_queries_executed_total)  		    as del_promscale_metrics_queries_executed_total,
+                sum(promscale_metrics_queries_timedout_total)  		    as del_promscale_metrics_queries_timedout_total,
+                sum(promscale_metrics_queries_failed_total)  		    as del_promscale_metrics_queries_failed_total,
+                sum(promscale_trace_query_requests_executed_total)      as del_promscale_trace_query_requests_executed_total,
+                sum(promscale_trace_dependency_requests_executed_total) as del_promscale_trace_dependency_requests_executed_total
+            FROM
+                deleted_rows
+        ) x
         WHERE is_counter_reset_row = TRUE;
 
-        -- Delete the stale rows.
-        DELETE FROM SCHEMA_PS_CATALOG.promscale_instance_information WHERE is_counter_reset_row = FALSE AND current_timestamp - last_updated > interval '1 HOUR';
+        RETURN TRUE;
     END;
 $$
 LANGUAGE PLPGSQL;
