e585aee6ffd21181d9ae9235fbe75ab43de95fa1
Ante Kresic <ante.kresic@gmail.com>
2021-12-20 13:22:47 +0100

    Update docker compose HA test to use new HA mechanism

--- docker-compose/high-availability/test.sh
+++ docker-compose/high-availability/test.sh
@@ -10,28 +10,36 @@ echo "running tests"
 #build latest image
 docker build -t timescale/promscale:latest ../.. --file ../../build/Dockerfile
 
-docker-compose -p promscale_deploy_ha up -d
+docker compose -p promscale_deploy_ha up -d
 
 cleanup() {
     if (( $? != 0 )); then
         echo "ERROR"
     fi
-    docker-compose -p promscale_deploy_ha down > /dev/null 2>&1
+    docker compose -p promscale_deploy_ha down > /dev/null 2>&1
 }
 
 trap cleanup EXIT
 
-last_line() {
-    LAST=`docker logs $1  2>&1| grep component=leader_election || true | tail -1`
-    echo $LAST
+ha_line() {
+    LAST=`docker exec $1 wget -O - localhost:9201/metrics-text 2>&1 | grep -i "promscale_ha_cluster_leader_info.*1$" | tail -n 1`
+    echo "$LAST"
 }
 
-is_role() {
-    LAST=$(last_line $1)
-    if [[ "$LAST" == *"status=$2"* ]]; then
+is_ingesting() {
+    LAST=$(ha_line $1)
+    if [[ "$LAST" == *"$1"* ]]; then
         true
     else
         false
+    fi
+}
+
+is_not_ingesting() {
+    if is_ingesting $1; then
+        false
+    else
+        true
     fi
 }
 
@@ -39,56 +47,58 @@ wait_for() {
     echo "waiting for $1"
 
     for i in `seq 10` ; do
-        if [ ! -z "$(last_line $1)" ] ; then
+        if [ ! -z "$(docker exec $1 wget -O - localhost:9201/metrics-text)" ] ; then
           echo "connector up"
           return 0
         fi
-        sleep 5
+        sleep 10
     done
     echo "FAIL waiting for $1"
     exit 1
 }
 
-wait_for_role() {
-    echo "waiting for $1 in role $2"
+wait_for_ingestion() {
+    echo "waiting for $1 to be in status $2"
 
     for i in `seq 10` ; do
-        if is_role "$1" "$2" ; then
-          echo "connector $1 is at role $2"
+        if $2 "$1"; then
+          echo "connector $1 $2"
           return
         fi
-        sleep 5
+        sleep 10
     done
-    echo "FAIL waiting for $1 in role $2"
+    echo "FAIL waiting for $1 $2"
     exit 1
 }
 
-wait_for "promscale_deploy_ha_promscale-connector1_1"
+wait_for "promscale_deploy_ha-promscale-connector1-1"
+wait_for "promscale_deploy_ha-promscale-connector2-1"
 
 #make sure inital conditions are what we expect
-if is_role "promscale_deploy_ha_promscale-connector1_1" "follower"; then
-    docker stop "promscale_deploy_ha_promscale-connector2_1"
-    wait_for_role "promscale_deploy_ha_promscale-connector1_1" "leader"
-    docker start "promscale_deploy_ha_promscale-connector2_1"
-    wait_for_role "promscale_deploy_ha_promscale-connector2_1" "follower"
+if is_not_ingesting "promscale_deploy_ha-promscale-connector1-1"; then
+    docker stop "promscale_deploy_ha-promscale-connector2-1"
+    wait_for_ingestion "promscale_deploy_ha-promscale-connector1-1" "is_ingesting"
+    docker start "promscale_deploy_ha-promscale-connector2-1"
+    wait_for_ingestion "promscale_deploy_ha-promscale-connector2-1"  "is_not_ingesting"
 fi
 
 echo "check initial condition"
-is_role "promscale_deploy_ha_promscale-connector1_1" "leader"
-is_role "promscale_deploy_ha_promscale-connector2_1" "follower"
+is_ingesting "promscale_deploy_ha-promscale-connector1-1" 
+is_not_ingesting "promscale_deploy_ha-promscale-connector2-1" 
 
 echo "kill connector1's prometheus"
-docker stop promscale_deploy_ha_prometheus1_1
-wait_for_role "promscale_deploy_ha_promscale-connector1_1" "follower"
-wait_for_role "promscale_deploy_ha_promscale-connector2_1" "leader"
+docker stop promscale_deploy_ha-prometheus1-1
+wait_for_ingestion "promscale_deploy_ha-promscale-connector2-1" "is_ingesting"
+wait_for_ingestion "promscale_deploy_ha-promscale-connector1-1" "is_not_ingesting"
 
-echo "bring prometheus 1 back and kill the current leader, connector 2 becomes leader again"
-docker start promscale_deploy_ha_prometheus1_1
-docker stop "promscale_deploy_ha_promscale-connector2_1"
-wait_for_role "promscale_deploy_ha_promscale-connector1_1" "leader"
+echo "bring prometheus 1 back and kill the current leader, connector 1 becomes leader again"
+docker start promscale_deploy_ha-prometheus1-1
+docker stop "promscale_deploy_ha-promscale-connector2-1"
+wait_for_ingestion "promscale_deploy_ha-promscale-connector1-1" "is_ingesting"
 
 echo "bring connector 2 up, it becomes follower"
-docker start "promscale_deploy_ha_promscale-connector2_1"
-wait_for_role "promscale_deploy_ha_promscale-connector2_1" "follower"
+docker start "promscale_deploy_ha-promscale-connector2-1"
+wait_for "promscale_deploy_ha-promscale-connector2-1"
+wait_for_ingestion "promscale_deploy_ha-promscale-connector2-1" "is_not_ingesting"
 
 echo "SUCCESS"
