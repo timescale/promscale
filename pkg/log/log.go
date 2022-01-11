00afea4aee25e2eb6639dcf39c845b86f4d716be
Ante Kresic <ante.kresic@gmail.com>
2022-01-11 08:40:48 +0100

    Add telemetry CLI flag section
    The introduction of this new section will reduce confusion between the telemetry
    configuration for the telemetry that Promscale produces and various other types
    of telemetry Promscale is used to store and query.

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -38,8 +38,8 @@ type Config struct {
 
 // ParseFlags parses the configuration flags for logging.
 func ParseFlags(fs *flag.FlagSet, cfg *Config) *Config {
-	fs.StringVar(&cfg.Level, "log.level", "info", "Log level to use from [ 'error', 'warn', 'info', 'debug' ].")
-	fs.StringVar(&cfg.Format, "log.format", "logfmt", "The log format to use [ 'logfmt', 'json' ].")
+	fs.StringVar(&cfg.Level, "telemetry.log.level", "info", "Log level to use from [ 'error', 'warn', 'info', 'debug' ].")
+	fs.StringVar(&cfg.Format, "telemetry.log.format", "logfmt", "The log format to use [ 'logfmt', 'json' ].")
 	return cfg
 }
 
