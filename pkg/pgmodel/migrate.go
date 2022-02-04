9aac24354094c6a63a5e53e10ec95074c4be13fd
John Pruitt <jgpruitt@gmail.com>
2022-02-04 15:40:55 -0600

    logic includes detecting the transition from old way to new way and taking special action in that case

--- pkg/pgmodel/migrate.go
+++ pkg/pgmodel/migrate.go
@@ -21,6 +21,10 @@ import (
 const (
 	firstAppVersionWithNewMigration = "0.11.0" // TODO decide the correct version to go here
 	firstExtVersionWithNewMigration = "0.5.0"  // TODO decide the correct version to go here
+
+	sqlDoesSchemaMigrationTableExist = "SELECT count(*) FILTER (WHERE tablename = 'prom_schema_migrations') > 0 FROM pg_tables"
+	sqlRemoveOldExtensionIfExists    = "DROP EXTENSION IF EXISTS promscale" // TODO to cascade or not to cascade?
+	sqlInstallExtensionAllBalls      = "CREATE EXTENSION promscale SCHEMA _prom_ext VERSION '0.0.0'"
 )
 
 var (
@@ -68,8 +72,8 @@ func Migrate(conn *pgx.Conn, appVersion VersionInfo, leaseLock *util.PgAdvisoryL
 		return errors.ErrInvalidSemverFormat
 	}
 
-	// old way of doing migrations via scripts embedded in the promscale client
 	if appSemver.LT(firstAppVersionWithNewMigration) {
+		// before the transition to doing all db migrations via the extension
 		err = oldMigration(conn, appSemver)
 		if err != nil {
 			return fmt.Errorf("Error while trying to migrate DB: %w", err)
@@ -80,12 +84,48 @@ func Migrate(conn *pgx.Conn, appVersion VersionInfo, leaseLock *util.PgAdvisoryL
 			return err
 		}
 	} else {
-		// new way of doing migrations via extension upgrades only
+		// the app version is after the transition to doing migrations via the extension
 
-		if err = removeOldExtensionIfExists(conn); err != nil {
+		exists, err := doesSchemaMigrationTableExist(conn)
+		if err != nil {
 			return err
 		}
 
+		if exists {
+			// if the prev app version was before the transition, then we need to:
+			// 1. make sure any remaining migrations app-side are applied
+			// 2. drop the old version of the extension if it exists
+			// 3. install the extension at version 0.0.0
+			// 4. upgrade the extension
+
+			// get the prev app version from reading the db table
+			prevAppSemver, err := getSchemaVersion(conn)
+			if err != nil {
+				return err
+			}
+
+			// prev < first and first <= app
+			// transition to the new world
+			// this *should only happen once
+			if prevAppSemver.LT(firstAppVersionWithNewMigration) {
+				// make sure any remaining migrations app-side are applied
+				err = oldMigration(conn, appSemver)
+				if err != nil {
+					return fmt.Errorf("Error while trying to migrate DB: %w", err)
+				}
+
+				// drop the old version of the extension if it exists
+				if err = removeOldExtensionIfExists(conn); err != nil {
+					return err
+				}
+				// install the extension at version 0.0.0
+				if err = installExtensionAllBalls(conn); err != nil {
+					return err
+				}
+			}
+		}
+
+		// new way of doing migrations via extension upgrades only
 		extOptions.Install = true // TODO decide whether it is appropriate to force this to true
 		extOptions.Upgrade = true // TODO decide whether it is appropriate to force this to true
 		_, err = extension.InstallUpgradePromscaleExtensions(conn, extOptions)
@@ -111,6 +151,14 @@ func oldMigration(db *pgx.Conn, appVersion semver.Version) (err error) {
 	return nil
 }
 
+func doesSchemaMigrationTableExist(db *pgx.Conn) (exists bool, err error) {
+	err = db.QueryRow(
+		context.Background(),
+		sqlDoesSchemaMigrationTableExist,
+	).Scan(&exists)
+	return
+}
+
 func removeOldExtensionIfExists(db *pgx.Conn) (err error) {
 	firstExtVersionWithNewMigration, err := semver.Make(firstExtVersionWithNewMigration)
 	if err != nil {
@@ -124,7 +172,7 @@ func removeOldExtensionIfExists(db *pgx.Conn) (err error) {
 	if installed && version.LT(firstExtVersionWithNewMigration) {
 		_, err := db.Exec(
 			context.Background(),
-			"DROP EXTENSION IF EXISTS promscale", // TODO to cascade or not to cascade?
+			sqlRemoveOldExtensionIfExists,
 		)
 		if err != nil {
 			return err
@@ -132,6 +180,14 @@ func removeOldExtensionIfExists(db *pgx.Conn) (err error) {
 	}
 
 	return nil
+}
+
+func installExtensionAllBalls(db *pgx.Conn) (err error) {
+	_, err = db.Exec(
+		context.Background(),
+		sqlInstallExtensionAllBalls,
+	)
+	return
 }
 
 // CheckDependencies makes sure all project dependencies, including the DB schema
