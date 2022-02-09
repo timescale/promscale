caf3ad65a1d90ebca4b17d397f6cbf55ad322f4c
John Pruitt <jgpruitt@gmail.com>
2022-02-09 10:53:15 -0600

    bumping the version to 0.11.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -38,8 +38,8 @@ var (
 	// It is customary to bump the version by incrementing the numeral after
 	// the `dev` tag. The SQL migration script name must correspond to the /new/ version.
 
-	Promscale                           = "0.9.0"
-	PrevReleaseVersion                  = "0.8.0"
+	Promscale                           = "0.11.0"
+	PrevReleaseVersion                  = "0.10.0"
 	PromMigrator                        = "0.0.3"
 	CommitHash                          = ""
 	EarliestUpgradeTestVersion          = "0.1.0"
