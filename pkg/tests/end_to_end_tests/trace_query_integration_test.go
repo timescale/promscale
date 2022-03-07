257dd941e498ba892d1dc3f2acce15b85cd21b83
Harkishen-Singh <harkishensingh@hotmail.com>
2022-03-07 16:25:37 +0530

    Update E2E tests to use internal Jaeger wrapper.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>
    
    This commits removes launching a new server inside trace_query
    intergration tests and instead reuses the server started in Promscale
    that directly serves the Jaeger JSON endpoints. This endpoint can be
    directly queried by Grafana/curl.

--- pkg/tests/end_to_end_tests/trace_query_integration_test.go
+++ pkg/tests/end_to_end_tests/trace_query_integration_test.go
@@ -9,25 +9,20 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net"
 	"net/http"
 	"sort"
 	"testing"
-	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
+	jaegerJSONModel "github.com/jaegertracing/jaeger/model/json"
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/stretchr/testify/require"
-	jaegerquery "github.com/timescale/promscale/pkg/jaeger/query"
+
+	promscaleJaeger "github.com/timescale/promscale/pkg/jaeger"
 	ingstr "github.com/timescale/promscale/pkg/pgmodel/ingestor"
 	"github.com/timescale/promscale/pkg/pgxconn"
 	"github.com/timescale/promscale/pkg/telemetry"
-
-	jaegerQueryApp "github.com/jaegertracing/jaeger/cmd/query/app"
-	jaegerQueryService "github.com/jaegertracing/jaeger/cmd/query/app/querysvc"
-	jaegerJSONModel "github.com/jaegertracing/jaeger/model/json"
 )
 
 type traceQuery struct {
@@ -67,15 +62,14 @@ func TestCompareTraceQueryResponse(t *testing.T) {
 		err = ingestor.IngestTraces(context.Background(), copyTraces(sampleTraces))
 		require.NoError(t, err)
 
-		// Create Promscale proxy to query traces data.
-		promscaleProxy, err := jaegerquery.New(pgxconn.NewPgxConn(db), telemetry.NewNoopEngine())
+		// Start Promscale's HTTP endpoint for Jaeger query.
+		server, err := promscaleJaeger.New(pgxconn.NewPgxConn(db), ":9203", telemetry.NewNoopEngine())
 		require.NoError(t, err)
-
-		shutdown := runPromscaleTraceQueryJSONServer(promscaleProxy, ":9203")
-		defer shutdown.Close()
+		go func() {
+			require.NoError(t, server.Start())
+		}()
 
 		promscaleClient := httpClient{"http://localhost:9203"}
-
 		jaegerResponse, err := loadJaegerQueryResponses()
 		require.NoError(t, err)
 
@@ -204,63 +198,6 @@ func convertToTraces(arr []interface{}) []jaegerJSONModel.Trace {
 		panic(err)
 	}
 	return tmp
-}
-
-// runPromscaleTraceQueryJSONServer starts a server that acts as a wrapper around promscale trace query module, responding in
-// Jaeger JSON format. It reuses Jaeger's JSON layer as a wrapper so that the outputs from Jaeger's `/api` endpoints
-// can be directly compared with Promscale's output from query endpoints.
-func runPromscaleTraceQueryJSONServer(proxy *jaegerquery.Query, port string) io.Closer {
-	promscaleQueryHandler := jaegerQueryApp.NewAPIHandler(jaegerQueryService.NewQueryService(
-		proxy,
-		proxy,
-		jaegerQueryService.QueryServiceOptions{}))
-
-	r := jaegerQueryApp.NewRouter()
-	promscaleQueryHandler.RegisterRoutes(r)
-
-	server := &http.Server{Handler: r}
-
-	listener, err := net.Listen("tcp", port)
-	if err != nil {
-		panic(err)
-	}
-	go func() {
-		if err := server.Serve(listener); err != nil && err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
-
-	// Wait for server to be up.
-	check := time.NewTicker(time.Second / 2)
-	timeout, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	for {
-		select {
-		case <-timeout.Done():
-			panic("Promscale trace JSON server could not be up within the given timeout")
-		case <-check.C:
-			// Simple get request.
-			req, err := http.NewRequestWithContext(timeout, "GET", fmt.Sprintf("http://localhost%s/api/services", port), nil)
-			if err != nil {
-				panic(err)
-			}
-
-			client := http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				panic(err)
-			}
-
-			bSlice, err := ioutil.ReadAll(resp.Body)
-			if err != nil || string(bSlice) == "" {
-				panic(fmt.Errorf("empty bSlice or err: %w", err))
-			}
-
-			if resp.StatusCode == http.StatusOK {
-				return server
-			}
-		}
-	}
 }
 
 // Note: Promscale responds by default in GRPC. But, Jaeger responds in JSON. Since the final rendering layer of UI
