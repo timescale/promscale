2c95810700e6a26c26efe9f3f0b5791c440da8a7
Matvey Arye <mat@timescale.com>
2022-03-09 10:14:04 -0800

    Change end-to-end tests to work on latest db
    The end-to-end tests should work on the most up to date db.
    This is easier to maintain and helps find bugs in latest releases.

--- scripts/end_to_end_tests.sh
+++ scripts/end_to_end_tests.sh
@@ -11,7 +11,7 @@ case "${unameOut}" in
     *)          ISMAC=0;;
 esac
 
-TIMESCALE_IMAGE=${1:-"timescale/timescaledb:1.7.4-pg12"}
+TIMESCALE_IMAGE=${1:-"timescale/timescaledb:latest-pg14"}
 SCRIPT_DIR=$(cd $(dirname ${0}) && pwd)
 ROOT_DIR=$(dirname ${SCRIPT_DIR})
 DB_URL="localhost:5432"
