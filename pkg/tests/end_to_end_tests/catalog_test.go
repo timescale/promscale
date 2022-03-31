4550d8d3885577689fa965853bef36288b72fd93
John Pruitt <jgpruitt@gmail.com>
2022-03-31 16:15:13 -0500

    Add metric and series tables to extension
    We want to be consistent and have all tables related to the extension to
    be owned by the extension, i.e. have an entry in pg_depend. Creating and
    dropping metrics will ADD/DROP the corresponding tables to/from the extension.
    
    The new function _prom_catalog.has_permission_on_table will return true for
    any table that belongs to the extension per pg_depends. Further, any chunk
    table belonging to a hypertable that belongs to the extension will also
    return true.
    
    This new function is used by prom_api.compress_chunk and prom_api.set_chunk_time_interval. Since promscale hypertables are owned by the superuser that owns the extension, these two functions need to be security definer, however we don't want these functions to be able to impact any arbitrary hypertable/chunk. We only want these two functions
    to be used on hypertables/chunks owned by the extension. Hence the need for _prom_catalog.has_permission_on_table.
    
    This commit adds tests for the _prom_catalog.has_permission_on_table function.

--- pkg/tests/end_to_end_tests/catalog_test.go
+++ pkg/tests/end_to_end_tests/catalog_test.go
@@ -12,10 +12,7 @@ import (
 	"github.com/timescale/promscale/pkg/pgxconn"
 )
 
-func TestPromscaleHypertablesView(t *testing.T) {
-	if !*useTimescaleDB {
-		t.Skip("testing _prom_catalog.promscale_hypertables needs TimescaleDB support")
-	}
+func TestHasPermissionOnTable(t *testing.T) {
 	withDB(t, *testDatabase, func(db *pgxpool.Pool, tb testing.TB) {
 		ts := generateSmallTimeseries()
 		ingestor, err := ingstr.NewPgxIngestorForTests(pgxconn.NewPgxConn(db), nil)
@@ -30,15 +27,27 @@ func TestPromscaleHypertablesView(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		qry := "select array_agg(row(hypertable_schema, hypertable_name) order by hypertable_schema, hypertable_name)::text from _prom_catalog.promscale_hypertables"
-		expected := `{"(_ps_trace,event)","(_ps_trace,link)","(_ps_trace,span)","(prom_data,firstMetric)","(prom_data,secondMetric)"}`
-		var actual string
-		err = db.QueryRow(context.Background(), qry).Scan(&actual)
+		_, err = db.Exec(context.Background(), "create table public.foo (bar int)")
 		if err != nil {
 			t.Fatal(err)
 		}
-		if expected != actual {
-			t.Errorf("_prom_catalog.promscale_hypertables did not produce the expected results. expected: %s actual %s", expected, actual)
+		tests := [][]interface{}{
+			{"_ps_trace.span", 1},
+			{`prom_data."firstMetric"`, 1},
+			{`prom_data."secondMetric"`, 1},
+			{"public.foo", 0},                // not a part of the extension
+			{"ps_tag.tag_op_not_equals", -1}, // not a table
+		}
+		qry := `select case _prom_catalog.has_permission_on_table($1::regclass) when true then 1 when false then 0 else -1 end`
+		var actual int
+		for _, expected := range tests {
+			err = db.QueryRow(context.Background(), qry, expected[0]).Scan(&actual)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if expected[1] != actual {
+				t.Errorf("_prom_catalog.has_permission_on_table did not produce the expected results. expected: %d actual %d", expected[1], actual)
+			}
 		}
 	})
 }
