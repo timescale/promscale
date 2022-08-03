ca91e2f40ba5835e22a94037cc722d51ee2522bc
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2022-08-03 18:02:49 +0530

    Add e2e test for Jaeger gRPC based remote store
    Signed-off-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>

---
+++ pkg/tests/end_to_end_tests/jaeger_store_test.go
@@ -0,0 +1,41 @@
+package end_to_end_tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	jaegertranslator "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/jaeger"
+	"github.com/stretchr/testify/require"
+
+	"github.com/timescale/promscale/pkg/jaeger/store"
+	jaegerstore "github.com/timescale/promscale/pkg/jaeger/store"
+	ingstr "github.com/timescale/promscale/pkg/pgmodel/ingestor"
+	"github.com/timescale/promscale/pkg/pgxconn"
+)
+
+// Similar to TestQueryTraces, but uses Jaeger span ingestion interface.
+func TestJaegerSpanIngestion(t *testing.T) {
+	withDB(t, "jaeger_span_store_e2e", func(db *pgxpool.Pool, t testing.TB) {
+		ingestor, err := ingstr.NewPgxIngestorForTests(pgxconn.NewPgxConn(db), nil)
+		require.NoError(t, err)
+		defer ingestor.Close()
+
+		jaegerStore := jaegerstore.New(pgxconn.NewQueryLoggingPgxConn(db), ingestor, &store.DefaultConfig)
+
+		batch, err := jaegertranslator.ProtoFromTraces(generateTestTrace())
+		require.NoError(t, err)
+		for _, b := range batch {
+			for _, s := range b.Spans {
+				// ProtoFromTraces doesn't populates span.Process because it is already been exposed by batch.Process.
+				// See https://github.com/jaegertracing/jaeger-idl/blob/05fe64e9c305526901f70ff692030b388787e388/proto/api_v2/model.proto#L152-L160
+				s.Process = b.Process
+				err = jaegerStore.SpanWriter().WriteSpan(context.Background(), s)
+				require.NoError(t, err)
+			}
+		}
+		getOperationsTest(t, jaegerStore)
+		findTraceTest(t, jaegerStore)
+		getDependenciesTest(t, jaegerStore)
+	})
+}
