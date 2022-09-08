6ec11315733ec9da019d5d60d823d207cdaaa40c
Valery Meleshkin <valeriy@timescale.com>
2022-09-08 17:00:41 +0200

    Introducing metrics for to-be-compressed and to-be-deleted chunks.

--- pkg/pgmodel/metrics/database/metrics.go
+++ pkg/pgmodel/metrics/database/metrics.go
@@ -97,6 +97,90 @@ var metrics = []metricQueryWrap{
 			prometheus.GaugeOpts{
 				Namespace: util.PromNamespace,
 				Subsystem: "sql_database",
+				Name:      "chunks_metrics_expired_count",
+				Help:      "The number of metrics chunks soon to be removed by maintenance jobs.",
+			},
+		),
+		query: `WITH conf AS MATERIALIZED (SELECT _prom_catalog.get_default_retention_period() AS def_retention)
+		SELECT count(*)::BIGINT
+		FROM _timescaledb_catalog.dimension_slice ds
+			 INNER JOIN _timescaledb_catalog.dimension d ON (d.id = ds.dimension_id)
+			 INNER JOIN _timescaledb_catalog.hypertable h ON (h.id = d.hypertable_id)
+			 INNER JOIN _prom_catalog.metric m ON (m.table_name = h.table_name AND m.table_schema = h.schema_name)
+			 JOIN conf ON TRUE
+		WHERE ds.range_start < _timescaledb_internal.time_to_internal(now() - coalesce(m.retention_period, conf.def_retention))
+		  AND ds.range_end < _timescaledb_internal.time_to_internal(now() - coalesce(m.retention_period, conf.def_retention))`,
+	}, {
+		metric: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: util.PromNamespace,
+				Subsystem: "sql_database",
+				Name:      "chunks_metrics_uncompressed_count",
+				Help:      "The number of metrics chunks soon to be compressed by maintenance jobs.",
+			},
+		),
+		query: `WITH chunk_candidates AS MATERIALIZED (
+		    SELECT chcons.dimension_slice_id, h.table_name, h.schema_name
+		    FROM _timescaledb_catalog.chunk_constraint chcons
+			     INNER JOIN _timescaledb_catalog.chunk c ON c.id = chcons.chunk_id
+			     INNER JOIN _timescaledb_catalog.hypertable h ON h.id = c.hypertable_id
+		    WHERE c.dropped IS FALSE
+		      AND h.compression_state = 1 -- compression_enabled = TRUE
+		      AND (c.status & 1) != 1) -- only check for uncompressed chunks
+		SELECT count(*)::BIGINT
+		FROM chunk_candidates cc
+			 INNER JOIN _timescaledb_catalog.dimension_slice ds ON ds.id = cc.dimension_slice_id
+			 INNER JOIN _prom_catalog.metric m ON (m.table_name = cc.table_name AND m.table_schema = cc.schema_name)
+		WHERE (m.delay_compression_until IS NULL OR m.delay_compression_until < now())
+		  AND NOT m.is_view
+		  AND ds.range_start <= _timescaledb_internal.time_to_internal(now() - interval '1 hour')
+		  AND ds.range_end <= _timescaledb_internal.time_to_internal(now() - interval '1 hour')`,
+	}, {
+		metric: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: util.PromNamespace,
+				Subsystem: "sql_database",
+				Name:      "chunks_traces_expired_count",
+				Help:      "The number of traces chunks soon to be removed by maintenance jobs.",
+			},
+		),
+		query: `WITH conf AS MATERIALIZED (SELECT coalesce(ps_trace.get_trace_retention_period(), interval '0 day') AS def_retention)
+		SELECT count(*)::BIGINT
+		FROM _timescaledb_catalog.dimension_slice ds
+			 INNER JOIN _timescaledb_catalog.dimension d ON (d.id = ds.dimension_id)
+			 INNER JOIN _timescaledb_catalog.hypertable h ON (h.id = d.hypertable_id)
+			 JOIN conf ON TRUE
+		WHERE ds.range_start < _timescaledb_internal.time_to_internal(now() - conf.def_retention)
+		  AND ds.range_end < _timescaledb_internal.time_to_internal(now() - conf.def_retention)
+		  AND h.schema_name = '_ps_trace'`,
+	}, {
+		metric: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: util.PromNamespace,
+				Subsystem: "sql_database",
+				Name:      "chunks_traces_uncompressed_count",
+				Help:      "The number of traces chunks soon to be compressed by maintenance jobs.",
+			},
+		),
+		query: `WITH chunk_candidates AS MATERIALIZED (
+			SELECT chcons.dimension_slice_id
+			FROM _timescaledb_catalog.chunk_constraint chcons
+				 INNER JOIN _timescaledb_catalog.chunk c ON c.id = chcons.chunk_id
+				 INNER JOIN _timescaledb_catalog.hypertable h ON h.id = c.hypertable_id
+			WHERE c.dropped IS FALSE
+			  AND h.schema_name = '_ps_trace'
+			  AND h.compression_state = 1 -- compression_enabled = TRUE
+			  AND (c.status & 1) != 1) -- only check for uncompressed chunks
+		    SELECT count(*)::BIGINT
+		    FROM chunk_candidates cc
+			     INNER JOIN _timescaledb_catalog.dimension_slice ds ON ds.id = cc.dimension_slice_id
+		    WHERE ds.range_start <= _timescaledb_internal.time_to_internal(now() - interval '1 hour')
+		      AND ds.range_end <= _timescaledb_internal.time_to_internal(now() - interval '1 hour')`,
+	}, {
+		metric: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace: util.PromNamespace,
+				Subsystem: "sql_database",
 				Name:      "compression_status",
 				Help:      "Compression status in TimescaleDB.",
 			},
@@ -128,7 +212,7 @@ var metrics = []metricQueryWrap{
 				Namespace: util.PromNamespace,
 				Subsystem: "sql_database",
 				Name:      "worker_maintenance_job_failed",
-				Help:      "Number of Promscale maintenance workers.",
+				Help:      "Number of Promscale maintenance jobs that failed.",
 			},
 		),
 		query: `select count(stats.last_run_status)
