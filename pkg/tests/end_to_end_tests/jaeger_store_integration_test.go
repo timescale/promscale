14865d27f94277a416b515a8fd39e7a64de1ec72
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2022-10-03 14:08:40 +0530

    Clone test span values before transferring to SpanWriter
    Jaeger storage integration tests passes span to SpanWriter and uses same span object to verify against result. We mutate given span to encode binary tags and this causes test change in test expectation.
    
    This commit adds a simple CopyingSpanWriter which copies tags before passing down to SpanWriter.
    
    Signed-off-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>

--- pkg/tests/end_to_end_tests/jaeger_store_integration_test.go
+++ pkg/tests/end_to_end_tests/jaeger_store_integration_test.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/jaegertracing/jaeger/model"
+	"github.com/jaegertracing/jaeger/storage/spanstore"
 	"github.com/stretchr/testify/require"
 
 	"github.com/timescale/promscale/pkg/jaeger/store"
@@ -17,6 +19,25 @@ import (
 	"github.com/timescale/promscale/pkg/pgxconn"
 	jaeger_integration_tests "github.com/timescale/promscale/pkg/tests/end_to_end_tests/jaeger_store_integration_tests"
 )
+
+type spanCopyingWriter struct {
+	w spanstore.Writer
+}
+
+// Copies Tags to avoid altering test expectation.
+func (f spanCopyingWriter) WriteSpan(ctx context.Context, span *model.Span) error {
+	spanCopy := *span
+	spanCopy.Tags = append([]model.KeyValue{}, span.Tags...)
+	spanCopy.References = append([]model.SpanRef{}, span.References...)
+	process := *span.Process
+	spanCopy.Process = &process
+	spanCopy.Process.Tags = append([]model.KeyValue{}, span.Process.Tags...)
+	spanCopy.Logs = append([]model.Log{}, span.Logs...)
+	for i := range span.Logs {
+		spanCopy.Logs[i].Fields = append([]model.KeyValue{}, span.Logs[i].Fields...)
+	}
+	return f.w.WriteSpan(ctx, &spanCopy)
+}
 
 // Similar to TestQueryTraces, but uses Jaeger span ingestion interface.
 func TestJaegerStorageIntegration(t *testing.T) {
@@ -54,27 +75,14 @@ func TestJaegerStorageIntegration(t *testing.T) {
 				}
 				si := jaeger_integration_tests.StorageIntegration{
 					SpanReader: jaegerStore.SpanReader(),
-					SpanWriter: writer,
+					SpanWriter: spanCopyingWriter{writer},
 					CleanUp: func() error {
 						_, err := db.Exec(context.Background(), `TRUNCATE TABLE _ps_trace.span`)
 						return err
 					},
 					Refresh: func() error { return nil },
 					SkipList: []string{
-						"FindTraces/Tags_in_one_spot_-_Tags",
-						"FindTraces/Tags_in_one_spot_-_Logs",
-						"FindTraces/Tags_in_one_spot_-_Process",
-						"FindTraces/default",
 						"FindTraces/Tags_\\+_Operation_name$",
-						"FindTraces/Tags_\\+_Operation_name_\\+_max_Duration$",
-						"FindTraces/Tags_\\+_Operation_name_\\+_Duration_range$",
-						"FindTraces/Tags_\\+_Duration_range$",
-						"FindTraces/Tags_\\+_max_Duration$",
-						"FindTraces/Multi-spot_Tags_\\+_Operation_name_\\+_max_Duration",
-						"FindTraces/Multi-spot_Tags_\\+_Operation_name_\\+_Duration_range",
-						"FindTraces/Multi-spot_Tags_\\+_Duration_range",
-						"FindTraces/Multi-spot_Tags_\\+_max_Duration",
-						"FindTraces/Multiple_Traces",
 					},
 				}
 				si.IntegrationTestAll(t.(*testing.T))
