63232a2d426c1a02236e980224b0e7d4be902102
niksajakovljevic <niksa.jakovljevic@gmail.com>
2023-01-10 16:54:42 +0100

    Reduce series cache size for E2E tests because of GH runner limitations

--- pkg/pgmodel/ingestor/ingestor.go
+++ pkg/pgmodel/ingestor/ingestor.go
@@ -101,8 +101,9 @@ func (ingestor *DBIngestor) IngestTraces(ctx context.Context, traces ptrace.Trac
 
 // IngestMetrics transforms and ingests the timeseries data into Timescale database.
 // input:
-//     req the WriteRequest backing tts. It will be added to our WriteRequest
-//         pool when it is no longer needed.
+//
+//	req the WriteRequest backing tts. It will be added to our WriteRequest
+//	    pool when it is no longer needed.
 func (ingestor *DBIngestor) IngestMetrics(ctx context.Context, r *prompb.WriteRequest) (numInsertablesIngested uint64, numMetadataIngested uint64, err error) {
 	if ingestor.closed.Load() {
 		return 0, 0, fmt.Errorf("ingestor is closed and can't ingest metrics")
